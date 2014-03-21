9ad02443ed06c631aa0e2410a0f0bdb9fbab3515
Jason Lantz <jlantz@salesforce.com>
2014-03-21 17:46:07 -0400

    Added script to automate updating of mpinstaller package dependencies using the version.properties file

---
+++ scripts/mpinstaller_update_dependencies.py
@@ -0,0 +1,47 @@
+import json
+import os
+import requests
+
+MPINSTALLER_BASE_URL=os.environ.get('MPINSTALLER_BASE_URL')
+PACKAGE_KEY=os.environ.get('PACKAGE_KEY')
+NAMESPACE=os.environ.get('NAMESPACE')
+BETA=os.environ.get('BETA', False)
+VERSION=os.environ.get('VERSION')
+PROPERTIES_PATH=os.environ.get('PROPERTIES_PATH', None)
+
+dependencies_url = '%s/%s/dependencies' % (MPINSTALLER_BASE_URL, NAMESPACE)
+if BETA:
+    dependencies_url = dependencies_url + '/beta'
+
+current_dependencies = json.loads(requests.get(dependencies_url).content)
+dependencies = []
+
+if PROPERTIES_PATH:
+    f = open(PROPERTIES_PATH, 'r')
+    for line in f.readlines():
+        namespace, version = [x.strip() for x in line.split('=')]
+        namespace = namespace.replace('version.','')
+        # Skip any namespace with "Not Installed" as a the version
+        if version == 'Not Installed':
+            continue
+        dependencies.append({'namespace': namespace, 'number': version})
+
+dependencies.append({'namespace': NAMESPACE, 'number': VERSION})
+
+changed = False
+for package in current_dependencies:
+    matched = False
+    for new in dependencies:
+        if new['namespace'] == package['namespace']:
+            matched = True
+            if new['number'] == package['number']:
+                print "No change for %s" % new['namespace']
+            else:
+                print "Changing %s from %s to %s" % (package['namespace'], package['number'], new['number'])
+                changed = True
+            break
+    if not matched:
+        print "No change for %s" % package['namespace']  
+
+if changed:
+    resp = requests.post(dependencies_url, data=json.dumps(dependencies), headers={'Authorization': PACKAGE_KEY})
