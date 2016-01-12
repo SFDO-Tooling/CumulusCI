0e67b848f0a46c3900c2c41b6132432833b173e8
Jason Lantz <jlantz@salesforce.com>
2016-01-12 10:59:28 -0600

    Fix init arguments for simple-salesforce to 0.68.1

--- ci/push/push_api.py
+++ ci/push/push_api.py
@@ -204,7 +204,7 @@ class SalesforcePushApi(object):
         if serverurl.find('test.salesforce.com') != -1:
             sandbox = True
     
-        self.sf = Salesforce(username=username, password=password, security_token='', sandbox=sandbox, sf_version='35.0')
+        self.sf = Salesforce(username=username, password=password, security_token='', sandbox=sandbox, version='35.0')
 
         # Change base_url to use the tooling api
         self.sf.base_url = self.sf.base_url + 'tooling/'
