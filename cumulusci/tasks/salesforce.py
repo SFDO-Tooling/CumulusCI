ddd31ec190c0f724d77190cb18be6f016b474982
Chris Polcyn <cpolcyn@salesforce.com>
2016-10-27 15:45:24 -0500

    added class to run Apex tests using Tooling API

--- cumulusci/tasks/salesforce.py
+++ cumulusci/tasks/salesforce.py
@@ -1,7 +1,10 @@
 import base64
+import cgi
+import io
 import logging
 import os
 import tempfile
+from time import sleep
 import zipfile
 
 from simple_salesforce import Salesforce
@@ -138,3 +141,211 @@ class DeployBundles(Deploy):
             return api(**options)
         else:
             return api()
+
+class RunApexTests(BaseSalesforceToolingApiTask):
+    task_options = {
+        'test_name_match': {
+            'description': ('Query to find Apex test classes to run ' +
+                '("%" is wildcard)'),
+            'required': True,
+        },
+        'test_name_exclude': {
+            'description': ('Query to find Apex test classes to exclude ' +
+                '("%" is wildcard)'),
+            'required': False,
+        },
+        'namespace': {
+            'description': 'Salesforce project namespace',
+            'required': False,
+        },
+        'poll_interval': {
+            'description': 'Time to wait between polling for Apex test status',
+            'required': False,
+        },
+        'junit_output': {
+            'description': 'File name for JUnit output',
+            'required': False,
+        },
+    }
+
+    def _decode_to_unicode(self, content):
+        if content:
+            try:
+                # Try to decode ISO-8859-1 to unicode
+                return content.decode('ISO-8859-1')
+            except UnicodeEncodeError:
+                # Assume content is unicode already
+                return content
+
+    def _get_test_classes(self):
+        namespace = self.options.get('namespace')
+        if namespace:
+            namespace = "'{}'".format(namespace)
+        else:
+            namespace = 'null'
+        # Split by commas to allow multiple class name matching options
+        test_name_match = self.options['test_name_match']
+        included_tests = []
+        for pattern in test_name_match.split(','):
+            if pattern:
+                included_tests.append("Name LIKE '{}'".format(pattern))
+        # Add any excludes to the where clause
+        test_name_exclude = self.options.get('test_name_exclude', '')
+        excluded_tests = []
+        for pattern in test_name_exclude.split(','):
+            if pattern:
+                excluded_tests.append("(NOT Name LIKE '{}')".format(pattern))
+        # Get all test classes for namespace
+        query = ('SELECT Id, Name FROM ApexClass ' +
+            'WHERE NamespacePrefix = {}'.format(namespace))
+        if included_tests:
+            query += ' AND ({})'.format(' OR '.join(included_tests))
+        if excluded_tests:
+            query += ' AND {}'.format(' AND '.join(excluded_tests))
+        # Run the query
+        self.logger.info('Running query: {}'.format(query))
+        result = self.tooling.query_all(query)
+        self.logger.info('Found {} test classes'.format(result['totalSize']))
+        return result
+
+    def _get_test_results(self, job_id, classes_by_id, results_by_class_name,
+                          classes_by_name):
+        result = self.tooling.query_all("SELECT StackTrace, Message, " +
+            "ApexLogId, AsyncApexJobId, MethodName, Outcome, ApexClassId, " +
+            "TestTimestamp FROM ApexTestResult " +
+            "WHERE AsyncApexJobId = '{}'".format(job_id))
+        counts = {
+            'Pass': 0,
+            'Fail': 0,
+            'CompileFail': 0,
+            'Skip': 0,
+        }
+        for record in result['records']:
+            class_name = classes_by_id[record['ApexClassId']]
+            results_by_class_name[class_name][record['MethodName']] = record
+            counts[record['Outcome']] += 1
+        test_results = []
+        class_names = results_by_class_name.keys()
+        class_names.sort()
+        for class_name in class_names:
+            class_id = classes_by_name[class_name]
+            duration = None
+            self.logger.info(u'Class: {}'.format(class_name))
+            method_names = results_by_class_name[class_name].keys()
+            method_names.sort()
+            for method_name in method_names:
+                result = results_by_class_name[class_name][method_name]
+                self.logger.info(u'\t{Outcome}: {MethodName}'.format(**result))
+                test_results.append({
+                    'Children': result.get('children', None),
+                    'ClassName': self._decode_to_unicode(class_name),
+                    'Method': self._decode_to_unicode(result['MethodName']),
+                    'Message': self._decode_to_unicode(result['Message']),
+                    'Outcome': self._decode_to_unicode(result['Outcome']),
+                    'StackTrace': self._decode_to_unicode(
+                        result['StackTrace']),
+                    'Stats': result.get('stats', None),
+                    'TestTimestamp': result.get('TestTimestamp', None),
+                })
+                if result['Outcome'] in ['Fail','CompileFail']:
+                    self.logger.info(u'\tMessage: {Message}'.format(**result))
+                    self.logger.info(u'\tStackTrace: {StackTrace}'.format(
+                        **result))
+        self.logger.info(u'-' * 80)
+        self.logger.info(u'Pass: {}  Fail: {}  CompileFail: {}  Skip: {}'
+            .format(
+            counts['Pass'],
+            counts['Fail'],
+            counts['CompileFail'],
+            counts['Skip'],
+        ))
+        self.logger.info(u'-' * 80)
+        if counts['Fail'] or counts['CompileFail']:
+            self.logger.info(u'-' * 80)
+            self.logger.info(u'Failing Tests')
+            self.logger.info(u'-' * 80)
+            counter = 0
+            for result in test_results:
+                if result['Outcome'] not in ['Fail', 'CompileFail']:
+                    continue
+                counter += 1
+                self.logger.info(u'{}: {}.{} - {}'.format(counter,
+                    result['ClassName'], result['Method'], result['Outcome']))
+                self.logger.info(u'\tMessage: {}'.format(result['Message']))
+                self.logger.info(u'\tStackTrace: {}'.format(
+                    result['StackTrace']))
+        return test_results
+
+
+    def _run_task(self):
+        result = self._get_test_classes()
+        if result['totalSize'] == 0:
+            return
+        classes_by_id = {}
+        classes_by_name = {}
+        trace_id = None
+        results_by_class_name = {}
+        classes_by_log_id = {}
+        logs_by_class_id = {}
+        for record in result['records']:
+            classes_by_id[record['Id']] = record['Name']
+            classes_by_name[record['Name']] = record['Id']
+            results_by_class_name[record['Name']] = {}
+        self.logger.info('Queuing tests for execution...')
+        ids = classes_by_id.keys()
+        job_id = self.tooling.restful('runTestsAsynchronous',
+            params={'classids': ','.join(str(id) for id in ids)})
+        self._wait_for_tests(job_id)
+        test_results = self._get_test_results(
+            job_id, classes_by_id, results_by_class_name, classes_by_name)
+        self._write_output(test_results)
+
+    def _wait_for_tests(self, job_id):
+        poll_interval = int(self.options.get('poll_interval', 1))
+        while True:
+            result = self.tooling.query_all(
+                "SELECT Id, Status, ApexClassId FROM ApexTestQueueItem " +
+                "WHERE ParentJobId = '{}'".format(job_id))
+            counts = {
+                'Aborted': 0,
+                'Completed': 0,
+                'Failed': 0,
+                'Holding': 0,
+                'Preparing': 0,
+                'Processing': 0,
+                'Queued': 0,
+            }
+            for record in result['records']:
+                counts[record['Status']] += 1
+            self.logger.info('Completed: {}  Processing: {}  Queued: {}'
+                .format(
+                counts['Completed'],
+                counts['Processing'],
+                counts['Queued'],
+            ))
+            if counts['Queued'] == 0 and counts['Processing'] == 0:
+                self.logger.info('Apex tests completed')
+                break
+            sleep(poll_interval)
+
+    def _write_output(self, test_results):
+        filename = self.options['junit_output']
+        with io.open(filename, mode='w', encoding='utf-8') as f:
+            f.write(u'<testsuite tests="{}">\n'.format(len(test_results)))
+            for result in test_results:
+                s = u'  <testcase classname="{}" name="{}"'.format(
+                    result['ClassName'], result['Method'])
+                if ('Stats' in result and result['Stats']
+                    and 'duration' in result['Stats']):
+                    s += ' time="{}"'.format(result['Stats']['duration'])
+                if result['Outcome'] in ['Fail','CompileFail']:
+                    s += '>\n'
+                    s += '    <failure type="{}">{}</failure>\n'.format(
+                        cgi.escape(result['StackTrace']),
+                        cgi.escape(result['Message']),
+                    )
+                    s += '  </testcase>\n'
+                else:
+                    s += ' />\n'
+                f.write(s)
+            f.write(u'</testsuite>')
--- cumulusci/tests/test_tasks.py
+++ cumulusci/tests/test_tasks.py
@@ -1,18 +1,20 @@
 import unittest
 
+from mock import patch
 import responses
 
 from cumulusci.core.config import BaseGlobalConfig
 from cumulusci.core.config import BaseProjectConfig
 from cumulusci.core.config import OrgConfig
 from cumulusci.core.config import TaskConfig
+from cumulusci.core.keychain import BaseProjectKeychain
 from cumulusci.tasks.salesforce import BaseSalesforceToolingApiTask
-
+from cumulusci.tasks.salesforce import RunApexTests
 
 class TestBaseSalesforceToolingApiTask(unittest.TestCase):
 
     def setUp(self):
-        self.api_version = 38.0
+        self.api_version = 36.0
         self.global_config = BaseGlobalConfig(
             {'project': {'package': {'api_version': self.api_version}}})
         self.project_config = BaseProjectConfig(self.global_config)
@@ -23,13 +25,73 @@ class TestBaseSalesforceToolingApiTask(unittest.TestCase):
         }
         self.task_config = TaskConfig()
         self.org_config = OrgConfig({
-            'instance_url': 'foo',
-            'access_token': 'bar',
+            'instance_url': 'example.com',
+            'access_token': 'abc123',
         })
+        self.base_tooling_url = 'https://{}/services/data/v{}/tooling/'.format(
+            self.org_config.instance_url, self.api_version)
 
     def test_get_tooling_object(self):
         task = BaseSalesforceToolingApiTask(
             self.project_config, self.task_config, self.org_config)
         obj = task._get_tooling_object('TestObject')
-        self.assertEqual(obj.base_url, 'https://foo/services/data/v' +
-            '{}/tooling/sobjects/TestObject/'.format(self.api_version))
+        url = self.base_tooling_url + 'sobjects/TestObject/'
+        self.assertEqual(obj.base_url, url)
+
+class TestRunApexTests(unittest.TestCase):
+
+    def setUp(self):
+        self.api_version = 36.0
+        self.global_config = BaseGlobalConfig(
+            {'project': {'api_version': self.api_version}})
+        self.task_config = TaskConfig()
+        self.task_config.config['options'] = {
+            'junit_output': 'test_junit_output.txt',
+            'poll_interval': 1,
+            'test_name_match': '%_TEST',
+        }
+        self.project_config = BaseProjectConfig(self.global_config)
+        self.project_config.config['project'] = {'package': {
+            'api_version': self.api_version}}
+        keychain = BaseProjectKeychain(self.project_config, '')
+        self.project_config.set_keychain(keychain)
+        self.org_config = OrgConfig({
+            'instance_url': 'example.com',
+            'access_token': 'abc123',
+        })
+        self.base_tooling_url = 'https://{}/services/data/v{}/tooling/'.format(
+            self.org_config.instance_url, self.api_version)
+
+    def _mock_apex_class_query(self):
+        url = self.base_tooling_url + 'query/'
+        expected_response = {
+            'done': True,
+            'records': [{
+                'ApexClassId': 1,
+                'Id': 1,
+                'Message': 'Test passed',
+                'MethodName': 'TestMethod',
+                'Name': 'TestClass_TEST',
+                'Outcome': 'Pass',
+                'StackTrace': '1. ParentFunction\n2. ChildFunction',
+                'Status': 'Completed',
+            }],
+            'totalSize': 1,
+        }
+        responses.add(responses.GET, url, json=expected_response)
+
+    def _mock_run_tests(self):
+        url = self.base_tooling_url + 'runTestsAsynchronous'
+        expected_response = {
+            'foo': 'bar',
+        }
+        responses.add(responses.GET, url, json=expected_response)
+
+    @responses.activate
+    def test_run_task(self):
+        self._mock_apex_class_query()
+        self._mock_run_tests()
+        task = RunApexTests(
+            self.project_config, self.task_config, self.org_config)
+        with patch.object(OrgConfig, 'refresh_oauth_token'):
+            task()
