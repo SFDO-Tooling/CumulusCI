9b664fc23b35b62c7263ed652cfbd3ee23e4558f
Chris Polcyn <cpolcyn@salesforce.com>
2017-03-22 15:50:00 -0500

    remove changes from a386604 and a2f9419

--- cumulusci/core/utils.py
+++ cumulusci/core/utils.py
@@ -3,8 +3,6 @@
 import_class: Task class defn import helper """
 
 import logging
-import os
-import sys
 
 
 def import_class(path):
@@ -12,12 +10,5 @@ def import_class(path):
     components = path.split('.')
     module = components[:-1]
     module = '.'.join(module)
-       
-    # Attempt to add pwd to path
-    try:
-        sys.path.append(os.getcwd())
-    except OSError:
-        pass
-
     mod = __import__(module, fromlist=[components[-1]])
     return getattr(mod, components[-1])
