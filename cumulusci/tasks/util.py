acbd2b5b44e9225a620e1d975e0c3e9b27954097
Chris Polcyn <cpolcyn@salesforce.com>
2017-03-31 12:08:38 -0500

    formatting

--- cumulusci/tasks/util.py
+++ cumulusci/tasks/util.py
@@ -4,6 +4,7 @@ import time
 from cumulusci.core.tasks import BaseTask
 from cumulusci.utils import download_extract_zip
 
+
 class DownloadZip(BaseTask):
     name = 'Download'
     task_options = {
@@ -16,19 +17,25 @@ class DownloadZip(BaseTask):
             'required': True,
         },
         'subfolder': {
-            'description': 'The subfolder of the target zip to extract.  Defaults to extracting the root of the zip file to the destination.',
+            'description': (
+                'The subfolder of the target zip to extract. Defaults to' +
+                ' extracting the root of the zip file to the destination.'
+            ),
         },
     }
-   
+
     def _run_task(self):
         if not self.options['dir']:
             self.options['dir'] = '.'
         elif not os.path.exists(self.options['dir']):
             os.makedirs(self.options['dir'])
-            
-        download_extract_zip(self.options['url'], self.options['dir'], self.options.get('subfolder'))
-         
-    
+
+        download_extract_zip(
+            self.options['url'],
+            self.options['dir'],
+            self.options.get('subfolder'),
+        )
+
 
 class Sleep(BaseTask):
     name = 'Sleep'
@@ -37,10 +44,11 @@ class Sleep(BaseTask):
             'description': 'The number of seconds to sleep',
             'required': True,
         },
-    } 
-    
-    def _run_task(self):
-        self.logger.info("Sleeping for {} seconds".format(self.options['seconds']))
-        time.sleep(float(self.options['seconds']))
-        self.logger.info("Done")
+    }
 
+    def _run_task(self):
+        self.logger.info(
+            'Sleeping for {} seconds'.format(self.options['seconds'])
+        )
+        time.sleep(float(self.options['seconds']))
+        self.logger.info('Done')
