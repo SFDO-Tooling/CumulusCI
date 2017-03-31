e594d8c1df462ac9ff785b11a298367763b8a6c4
Chris Polcyn <cpolcyn@salesforce.com>
2017-03-31 12:26:58 -0500

    add task to list metadata types in project

--- cumulusci/tasks/util.py
+++ cumulusci/tasks/util.py
@@ -1,5 +1,6 @@
 import os
 import time
+from xml.dom.minidom import parse
 
 from cumulusci.core.tasks import BaseTask
 from cumulusci.utils import download_extract_zip
@@ -34,6 +35,40 @@ class DownloadZip(BaseTask):
             self.options['url'],
             self.options['dir'],
             self.options.get('subfolder'),
+        )
+
+
+class ListMetadataTypes(BaseTask):
+    name = 'ListMetadataTypes'
+    task_options = {
+        'package_xml': {'description': (
+            'The project package.xml file.' +
+            ' Defaults to <project_root>/src/package.xml'
+        )}
+    }
+
+    def _init_options(self, kwargs):
+        super(ListMetadataTypes, self)._init_options(kwargs)
+        if 'package_xml' not in self.options:
+            self.options['package_xml'] = os.path.join(
+                self.project_config.repo_root,
+                'src',
+                'package.xml',
+            )
+
+    def _run_task(self):
+        dom = parse(self.options['package_xml'])
+        package = dom.getElementsByTagName('Package')[0]
+        types = package.getElementsByTagName('types')
+        type_list = []
+        for t in types:
+            name = t.getElementsByTagName('name')[0]
+            metadata_type = name.firstChild.nodeValue
+            type_list.append(metadata_type)
+        self.logger.info(
+            'Metadata types found in %s:\r\n%s',
+            self.options['package_xml'],
+            '\r\n'.join(type_list),
         )
 
 
