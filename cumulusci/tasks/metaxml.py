6c9b47af799af7757270cf616a6783b40276e15f
Chris Polcyn <cpolcyn@salesforce.com>
2017-06-06 11:46:50 -0700

    new task to set API version in *meta.xml files

--- cumulusci/tasks/metaxml.py
+++ cumulusci/tasks/metaxml.py
@@ -5,7 +5,57 @@ from lxml import etree as ET
 from cumulusci.core.tasks import BaseTask
 
 
-class UpdateDependencies(BaseTask):
+class MetaXmlBaseTask(BaseTask):
+
+    def _init_options(self, kwargs):
+        super(MetaXmlBaseTask, self)._init_options(kwargs)
+        if 'dir' not in self.options or not self.options['dir']:
+            self.options['dir'] = os.path.join(
+                self.project_config.repo_root,
+                'src',
+            )
+
+    def _run_task(self):
+        for root, dirs, files in os.walk(self.options['dir']):
+            for filename in files:
+                if filename.endswith('-meta.xml'):
+                    self._process_file(os.path.join(root, filename))
+
+
+class UpdateApi(MetaXmlBaseTask):
+    task_options = {
+        'dir': {
+            'description': 'Base directory to search for *-meta.xml files',
+        },
+        'version': {
+            'description': 'API version number e.g. 37.0',
+            'required': True,
+        },
+    }
+
+    def _process_file(self, filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        xmlns = re.search('({.+}).+', root.tag).group(1)
+        changed = False
+        api_version = root.find('{}apiVersion'.format(xmlns))
+        if (api_version is not None and
+                api_version.text != self.options['version']):
+            api_version.text = self.options['version']
+            changed = True
+        if changed:
+            tree.write(
+                filename,
+                xml_declaration=True,
+                encoding='UTF-8',
+                pretty_print=True,
+            )
+            self.logger.info('Processed file %s', filename)
+        else:
+            self.logger.info('No changes for file %s', filename)
+
+
+class UpdateDependencies(MetaXmlBaseTask):
     task_options = {
         'dir': {
             'description': 'Base directory to search for *-meta.xml files',
@@ -20,35 +70,24 @@ class UpdateDependencies(BaseTask):
         },
     }
 
-    def _init_options(self, kwargs):
-        super(UpdateDependencies, self)._init_options(kwargs)
-        if 'dir' not in self.options or not self.options['dir']:
-            self.options['dir'] = os.path.join(
-                self.project_config.repo_root,
-                'src',
-            )
-        self.v_major, self.v_minor = self.options['version'].split('.')
-
-    def _run_task(self):
-        for root, dirs, files in os.walk(self.options['dir']):
-            for filename in files:
-                if filename.endswith('-meta.xml'):
-                    self._process_file(os.path.join(root, filename))
-
     def _process_file(self, filename):
         tree = ET.parse(filename)
         root = tree.getroot()
         xmlns = re.search('({.+}).+', root.tag).group(1)
         changed = False
+        v_major, v_minor = self.options['version'].split('.')
         for package_version in root.findall('{}packageVersions'.format(xmlns)):
             namespace = package_version.find('{}namespace'.format(xmlns)).text
             if namespace != self.options['namespace']:
                 continue
-            changed = True
             major = package_version.find('{}majorNumber'.format(xmlns))
-            major.text = self.v_major
+            if major.text != v_major:
+                major.text = v_major
+                changed = True
             minor = package_version.find('{}minorNumber'.format(xmlns))
-            minor.text = self.v_minor
+            if minor.text != v_minor:
+                minor.text = v_minor
+                changed = True
         if changed:
             tree.write(
                 filename,
