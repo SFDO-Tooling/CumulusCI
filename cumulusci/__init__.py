39cb200c406b8f3fd89b99687b316b4753baf9f5
Jason Lantz <jasontlantz@gmail.com>
2017-08-28 11:47:41 -0500

    beta57

--- cumulusci/__init__.py
+++ cumulusci/__init__.py
@@ -1,4 +1,4 @@
 import os
 __import__('pkg_resources').declare_namespace('cumulusci')
-__version__ = '2.0.0-beta56'
+__version__ = '2.0.0-beta57'
 __location__ = os.path.dirname(os.path.realpath(__file__))
