69fcd20b0b363989df88d5e1ab9395d72001df41
Jason Lantz <jlantz@salesforce.com>
2017-09-07 15:27:29 -0500

    Don't go up to /app

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -9,8 +9,8 @@ nosetests --with-tap --tap-stream --with-coverage --cover-package=cumulusci
 coveralls
 
 # Clone the CumulusCI-Test repo to run test builds against it with cci
-cd ..
 git clone https://github.com/SalesforceFoundation/CumulusCI-Test
+cd CumulusCI-Test
 cci flow run ci_feature --org scratch --delete-org
 cci flow run ci_master --org packaging
 cci flow run release_beta --org packaging
