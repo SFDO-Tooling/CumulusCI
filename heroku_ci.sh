1666b2d11a7bf21f0a3b4c18d7f0d899ecbb987d
Jason Lantz <jlantz@salesforce.com>
2017-09-07 17:02:12 -0500

    Actually install the cloned repo before running cci functional tests

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -7,6 +7,9 @@ cd CumulusCI
 git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
 nosetests --with-tap --tap-stream --with-coverage --cover-package=cumulusci
 
+python setup.py build
+python setup.py install
+
 # Clone the CumulusCI-Test repo to run test builds against it with cci
 echo "------------------------------------------"
 echo "Running test builds against CumulusCI-Test"
@@ -17,7 +20,7 @@ git clone https://github.com/SalesforceFoundation/CumulusCI-Test
 cd CumulusCI-Test
 if [ $HEROKU_TEST_RUN_BRANCH == "master" ]; then
     echo "1...4"
-    coverage run --append --source=/app/cumulusci `which cci` flow run ci_feature --org scratch --delete-org | tee cci.log
+    coverage run --append --source=../cumulusci `which cci` flow run ci_feature --org scratch --delete-org | tee cci.log
     exit_status=$?
     if [ "$exit_status" == "0" ]; then
         echo "ok 1 - Successfully ran ci_feature"
@@ -25,21 +28,21 @@ if [ $HEROKU_TEST_RUN_BRANCH == "master" ]; then
         echo "not ok 1 - Failed ci_feature: `tail -1 cci.log`"
     fi
         
-    coverage run --append --source=/app/cumulusci `which cci` flow run ci_master --org packaging | tee -a cci.log
+    coverage run --append --source=../cumulusci `which cci` flow run ci_master --org packaging | tee -a cci.log
     exit_status=$?
     if [ "$exit_status" == "0" ]; then
         echo "ok 2 - Successfully ran ci_master"
     else
         echo "not ok 2 - Failed ci_master: `tail -1 cci.log`"
     fi
-    coverage run --append --source=/app/cumulusci `which cci` flow run release_beta --org packaging | tee -a cci.log
+    coverage run --append --source=../cumulusci `which cci` flow run release_beta --org packaging | tee -a cci.log
     exit_status=$?
     if [ "$exit_status" == "0" ]; then
         echo "ok 3 - Successfully ran release_beta"
     else
         echo "not ok 3 - Failed release_beta: `tail -1 cci.log`"
     fi
-    coverage run --append --source=/app/cumulusci `which cci` flow run ci_beta --org scratch --delete-org | tee -a cci.log
+    coverage run --append --source=../cumulusci `which cci` flow run ci_beta --org scratch --delete-org | tee -a cci.log
     exit_status=$?
     if [ "$exit_status" == "0" ]; then
         echo "ok 4 - Successfully ran ci_beta"
@@ -49,7 +52,7 @@ if [ $HEROKU_TEST_RUN_BRANCH == "master" ]; then
 
 else
     echo "1...1"
-    coverage run --append --source=/app/cumulusci `which cci` flow run ci_feature --org scratch --delete-org
+    coverage run --append --source=../cumulusci `which cci` flow run ci_feature --org scratch --delete-org
     exit_status=$?
     if [ "$exit_status" == "0" ]; then
         echo "ok 1 - Successfully ran ci_feature"
