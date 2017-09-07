5d0f0d3c3bfa1057a5325cdafa0f9a5c91ef3301
Jason Lantz <jlantz@salesforce.com>
2017-09-07 15:20:42 -0500

    Add buildpacks to test environment?

--- heroku_ci.sh
+++ heroku_ci.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # This script runs the tests on Heroku CI
 
+# Run the CumulusCI Unit Tests
 git clone -b "$HEROKU_TEST_RUN_BRANCH" --single-branch https://github.com/SalesforceFoundation/CumulusCI 
 cd CumulusCI
 git reset --hard $HEROKU_TEST_RUN_COMMIT_VERSION
 nosetests --with-tap --tap-stream --with-coverage --cover-package=cumulusci
 coveralls
+
+# Clone the CumulusCI-Test repo to run test builds against it with cci
+cd ..
+git clone https://github.com/SalesforceFoundation/CumulusCI-Test
+cci flow run ci_feature --org scratch --delete-org
+cci flow run ci_master --org packaging
+cci flow run release_beta --org packaging
+cci flow run ci_beta --org scratch --delete-org
