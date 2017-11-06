be7e983e83b3bf74d60307d9a7b4e51481e30b89
Chris Polcyn <cpolcyn@salesforce.com>
2017-11-06 12:23:41 -0800

    v2.0.0b71

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ test_requirements = [
 
 setup(
     name='cumulusci',
-    version='2.0.0-beta70',
+    version='2.0.0-beta71',
     description="Build and release tools for Salesforce developers",
     long_description=readme + '\n\n' + history,
     author="Jason Lantz",
