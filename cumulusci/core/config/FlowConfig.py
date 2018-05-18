a998e4d1bb043994493f446fd96d8f1cd9ddc2f6
Chris Polcyn <cpolcyn@salesforce.com>
2018-05-18 09:15:52 -0700

    split config.py into separate files per class

---
+++ cumulusci/core/config/FlowConfig.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from cumulusci.core.config import BaseConfig
+
+
+class FlowConfig(BaseConfig):
+    """ A flow with its configuration merged """
+    pass
---
+++ cumulusci/core/config/ServiceConfig.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from cumulusci.core.config import BaseConfig
+
+
+class ServiceConfig(BaseConfig):
+    pass
