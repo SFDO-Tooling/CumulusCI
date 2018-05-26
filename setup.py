aa827b2cbb13fcd7f78464dfd0c9bf25af7617aa
Christian Carter <christian.carter@salesforce.com>
2018-05-26 12:14:15 -0700

    setup.py runs core tests

--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
+    test_suite='cumulusci.core.tests',
     tests_require=test_requirements
 )
