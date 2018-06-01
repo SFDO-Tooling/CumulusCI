5ed5cb74fbdb38df409a65c0eacbfa6cf5f305f7
Jason Lantz <jlantz@salesforce.com>
2018-05-31 20:58:01 -0500

    beta99

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ test_requirements = [
 
 setup(
     name='cumulusci',
-    version='2.0.0-beta98',
+    version='2.0.0-beta99',
     description="Build and release tools for Salesforce developers",
     long_description=readme + '\n\n' + history,
     author="Jason Lantz",
