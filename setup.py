098bf787956836598f4f93eca6970de23d6cfa35
David Glick <dglick@salesforce.com>
2018-07-27 11:10:58 -0400

    Only claim we support Python versions that we do

--- setup.py
+++ setup.py
@@ -85,12 +85,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='cumulusci.core.tests',
     tests_require=test_requirements
