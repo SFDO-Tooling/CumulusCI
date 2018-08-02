71d0c12e74211ff1e1b3a8642e5133817208719b
Christian Carter <christian.carter@salesforce.com>
2018-08-02 15:22:38 -0700

    good pass at starting a packagebuilder

--- cumulusci/core/processors.py
+++ cumulusci/core/processors.py
@@ -4,6 +4,24 @@ import os
 import base64
 import zipfile
 
+from cumulusci.utils import memoize
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+def files_from_path(rootDir):
+    """ list all the files in rootDir, with their path relative to rootDir."""
+
+    # stolen from stackexchange.... its a gnarly list comprehension but
+    # pythons like, designed to work this way somehow.
+    # https://stackoverflow.com/questions/1192978/python-get-relative-path-of-all-files-and-subfolders-in-a-directory
+
+    return [os.path.relpath(os.path.join(dirpath, fname), rootDir)
+            for (dirpath, _, filenames)
+            in os.walk(rootDir)
+            for fname in filenames]
+
 
 class BasePackageZipBuilder(object):
     def __init__(self):
@@ -18,51 +36,66 @@ class BasePackageZipBuilder(object):
         self._write_file('package.xml', package_xml)
 
     def _write_file(self, path, content):
+        if self.zip is None:
+            raise RuntimeError('Attempt to write without opening the zip.')
         self.zip.writestr(path, content)
 
     def _encode_zip(self):
         if not self.zip.fp:
-            raise RuntimeError('Attempt to encode a file that was already closed')
+            raise RuntimeError(
+                'Attempt to encode a file that was already closed')
         self.zip.close()
         return base64.b64encode(self._stream.getvalue())
 
-class CallablePackageZipBuilder(BasePackageZipBuilder):
-    def __call__(self):
+    def _populate_zip(self):
+        raise NotImplementedError(
+            'Subclasses need to provide their own implementation')
+
+    def encode_zip(self):
         self._open_zip()
         self._populate_zip()
         return self._encode_zip()
 
-    def _populate_zip(self):
-        raise NotImplementedError('Subclasses need to provide their own implementation')
+
+class CallablePackageZipBuilder(BasePackageZipBuilder):
+    def __call__(self):
+        return self.encode_zip()
+
 
 class FilePackageZipBuilder(BasePackageZipBuilder):
     """ Builds a package.zip from a given MDAPI formatted source tree """
+
     def __init__(self, path):
         super(FilePackageZipBuilder, self).__init__()
         self.path = path
+        self._contents = {}
+        self._load_files()
 
-"""
-get a path
-find all members
-generate a packagexml
-load members into zipfile
-encode the zipfile
-"""
+    @property
+    @memoize
+    def _file_list(self):
+        return [f for f in files_from_path(self.path) if not os.path.basename(f).startswith('.')]
 
+    def _load_files(self):
+        for filename in self._file_list:
+            with open(os.path.join(self.path, filename), 'r') as f:
+                self._contents[filename] = f.read()
 
-class MetadataProcessor(object):
-    """
-    MetadataProcessor allows a caller (task) to:
-    - walk a metadata tree (presumably on the filesystem)
-    - process each item (for example to remove a version dependency) with pre-registered processors
-    - get back a zipfile ready for the salesforce metadata api
+    def _populate_zip(self):
+        for filename in self._file_list:
+            self._write_file(filename, self._contents[filename])
 
-    This lets us replace core functionality of utils, as well as packagezipbuilder.
-    """
+#
+#get a path
+#find all members
+#generate a packagexml
+#load members into zipfile
+#encode the zipfile
 
-    def process(self):
-        for root, dirs, files in os.walk(self.path):
-
-            for fname in files:
-
-                fpath = os.path.join(root, fname)
+#   MetadataProcessor allows a caller (task) to:
+#    - walk a metadata tree (presumably on the filesystem)
+#    - process each item (for example to remove a version dependency) with pre-registered processors
+#    - get back a zipfile ready for the salesforce metadata api
+#
+#    This lets us replace core functionality of utils, as well as packagezipbuilder.
+#
--- cumulusci/core/tests/test_processors.py
+++ cumulusci/core/tests/test_processors.py
@@ -1,35 +1,53 @@
 from __future__ import absolute_import
 
 import io
+import os
 import base64
 import zipfile
 import unittest
 
 import nose
+from cumulusci.core import __location__
+from ..processors import CallablePackageZipBuilder, FilePackageZipBuilder
 
-from ..processors import MetadataProcessor
-from ..processors import CallablePackageZipBuilder
 
 class FakeZipBuilder(CallablePackageZipBuilder):
     def _populate_zip(self):
         self._write_file('test.txt', 'this is a test file')
+
 
 class TestPackageZipBuilders(unittest.TestCase):
     @nose.tools.raises(NotImplementedError)
     def test_raises_error(self):
         builder = CallablePackageZipBuilder()
         builder()
-    
+
     def test_returns_b64encoded_zip(self):
         builder = FakeZipBuilder()
         decodedzip = base64.b64decode(builder())
-        zipf = zipfile.ZipFile(io.BytesIO(decodedzip),'r')
-        self.assertIsNone(zipf.testzip()) # testzip returns None
+        zipf = zipfile.ZipFile(io.BytesIO(decodedzip), 'r')
+        self.assertIsNone(zipf.testzip())  # testzip returns None
 
 
+class TestFilePackageZipBuilder(unittest.TestCase):
+    src_dir = os.path.join(__location__, 'tests/mdapisrc/ccitest')
+    with open(os.path.join(src_dir, 'tabs/SampleObject__c.tab'), 'r') as f:
+        tab_contents = f.read()
 
-class TestMetadataProcessor(unittest.TestCase):
-    def test_bytefile(self):
-        processor = MetadataProcessor('mdapisrc')
-        #self.assertEqual(processor.path, 'mdapisc')
-    
+    def test_builds_package(self):
+        builder = FilePackageZipBuilder(self.src_dir)
+        decodedzip = base64.b64decode(builder.encode_zip())
+        zipf = zipfile.ZipFile(io.BytesIO(decodedzip), 'r')
+        self.assertIsNone(zipf.testzip())  # testzip returns None
+
+    def test_package_has_members(self):
+        builder = FilePackageZipBuilder(self.src_dir)
+        builder.encode_zip()
+        zipf = builder.zip
+        self.assertIn('tabs/SampleObject__c.tab', zipf.namelist())
+
+    def test_package_members_are_equal(self):
+        builder = FilePackageZipBuilder(self.src_dir)
+        decodedzip = base64.b64decode(builder.encode_zip())
+        zipf = zipfile.ZipFile(io.BytesIO(decodedzip), 'r')
+        self.assertEqual(zipf.read('tabs/SampleObject__c.tab'), self.tab_contents)
