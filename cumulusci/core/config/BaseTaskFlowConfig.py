aecf0b6e22cbbef4b3d69c8e4ae6d0651b2c243e
Christian Carter <christian.carter@salesforce.com>
2018-08-15 12:59:24 -0700

    factor out list_infos so that adjusting the cli is easier

--- cumulusci/core/config/BaseTaskFlowConfig.py
+++ cumulusci/core/config/BaseTaskFlowConfig.py
@@ -5,22 +5,25 @@ from cumulusci.core.config.FlowConfig import FlowConfig
 from cumulusci.core.config.TaskConfig import TaskConfig
 from cumulusci.core.exceptions import TaskNotFoundError, FlowNotFoundError
 
+def list_infos(infos):
+    rv = []
+    for info_name in infos:
+        info = infos[info_name]
+        if not info:
+            info = {}
+        rv.append({
+            'name': info_name,
+            'description': info.get('description', '')
+        })
+    return rv
+
 
 class BaseTaskFlowConfig(BaseConfig):
     """ Base class for all configs that contain tasks and flows """
 
     def list_tasks(self):
         """ Returns a list of task info dictionaries with keys 'name' and 'description' """
-        tasks = []
-        for task in self.tasks:
-            task_info = self.tasks[task]
-            if not task_info:
-                task_info = {}
-            tasks.append({
-                'name': task,
-                'description': task_info.get('description'),
-            })
-        return tasks
+        return list_infos(self.tasks)
 
     def get_task(self, name):
         """ Returns a TaskConfig """
@@ -31,16 +34,7 @@ class BaseTaskFlowConfig(BaseConfig):
 
     def list_flows(self):
         """ Returns a list of flow info dictionaries with keys 'name' and 'description' """
-        flows = []
-        for flow in self.flows:
-            flow_info = self.flows[flow]
-            if not flow_info:
-                flow_info = {}
-            flows.append({
-                'name': flow,
-                'description': flow_info.get('description')
-            })
-        return flows
+        return list_infos(self.flows)
 
     def get_flow(self, name):
         """ Returns a FlowConfig """
