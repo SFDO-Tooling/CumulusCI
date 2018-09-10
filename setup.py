e227361fb525365191aa6853d1b5bff5007461b7
David Glick <dglick@salesforce.com>
2018-09-10 11:10:27 -0400

    prepare 2.0.9

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ test_requirements = [
 
 setup(
     name='cumulusci',
-    version='2.0.8',
+    version='2.0.9',
     description="Build and release tools for Salesforce developers",
     long_description=readme + '\n\n' + history,
     author="Salesforce.org",
