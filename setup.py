3d5d864e54703f80b50c4e58b091fd4f40f73d13
David Glick <dglick@salesforce.com>
2019-01-22 16:03:35 -0500

    fix?

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     author="Salesforce.org",
     author_email="jlantz@salesforce.com",
     url="https://github.com/SFDO-Tooling/CumulusCI",
-    packages=list(find_packages(".", "cumulusci")),
+    packages=list(find_packages("cumulusci", "cumulusci")),
     package_dir={"cumulusci": "cumulusci"},
     entry_points={"console_scripts": ["cci=cumulusci.cli.cci:main"]},
     include_package_data=True,
