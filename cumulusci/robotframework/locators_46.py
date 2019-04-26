afda324a7fd750d3aa39eff6819460a7a6e7ed5d
Bryan Oakley <bryan.oakley@gmail.com>
2019-04-25 21:00:53 -0500

    Tweaked one locator that was failing to work with Summer 19. I've run all of the CumulusCI and NPSP robot tests against Summer 19 prerelease and don't think any of the failures I saw on NPSP were related to the cumulusci locators.

--- cumulusci/robotframework/locators_46.py
+++ cumulusci/robotframework/locators_46.py
@@ -1,8 +1,12 @@
 from cumulusci.robotframework import locators_45
 
 lex_locators = locators_45.lex_locators.copy()
-lex_locators.update(
-    {
-        # put new locators or modified locators here
-    }
+
+# oof. This is gnarly.
+# Apparently, in 45 all modal buttons are in a class named 'modal-footer'
+# but in 46 some are in a class named 'actionsContainer' instead.
+lex_locators["modal"]["button"] = "{}{}{}".format(
+    "//div[contains(@class,'uiModal')]",
+    "//div[contains(@class,'modal-footer') or contains(@class, 'actionsContainer')]",
+    "//button[.//span[text()='{}']]",
 )
