be28ebaea9395ea67f3836798eedc99184c03dd7
Jason Lantz <jlantz@salesforce.com>
2019-05-07 16:30:09 -0500

    Support the ability to snapshot the found changes in list_changes

--- cumulusci/tasks/salesforce/sourcetracking.py
+++ cumulusci/tasks/salesforce/sourcetracking.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
+import json
 import os
 import re
 import xmltodict
 
+from cumulusci.core.utils import process_bool_arg
 from cumulusci.core.utils import process_list_arg
 from cumulusci.salesforce_api.metadata import ApiRetrieveUnpackaged
 from cumulusci.tasks.salesforce import BaseRetrieveMetadata
@@ -17,12 +19,19 @@ class ListChanges(BaseSalesforceApiTask):
         "include": {"description": "Include changed components matching this string."},
         "exclude": {"description": "Exclude changed components matching this string."},
         "exclude_namespace": {"description": "Namespaces that should be excluded."},
+        "snapshot": {
+            "description": "If True, all matching items will be set to be ignored at their current revision number.  This will exclude them from the results unless a new edit is made."
+        },
     }
 
     def _init_options(self, kwargs):
         super(ListChanges, self)._init_options(kwargs)
         self.options["include"] = process_list_arg(self.options.get("include", []))
         self.options["exclude"] = process_list_arg(self.options.get("exclude", []))
+        self.options["reset_status"] = process_bool_arg(
+            self.options.get("reset_status", [])
+        )
+        self.options["snapshot"] = process_bool_arg(self.options.get("snapshot", []))
         self.options["exclude_namespace"] = process_list_arg(
             self.options.get("exclude_namespace", [])
         )
@@ -33,11 +42,30 @@ class ListChanges(BaseSalesforceApiTask):
         if self.project_config.project__source__ignore:
             self._exclude.extend(self.project_config.project__source__ignore)
 
+        self._load_retrieve_status()
+
+    @property
+    def _retrieve_status_path(self):
+        parent_dir = os.path.join(".cci", "retrieve_status")
+        if not os.path.isdir(parent_dir):
+            os.makedirs(parent_dir)
+        return os.path.join(parent_dir, "{}.json".format(self.org_config.name))
+
+    def _load_retrieve_status(self):
+        self._retrieve_status = {}
+        if os.path.isfile(self._retrieve_status_path):
+            with open(self._retrieve_status_path, "r") as f:
+                self._retrieve_status = json.load(f)
+
     def _get_changes(self):
         changes = self.tooling.query(
-            "SELECT MemberName, MemberType FROM SourceMember WHERE IsNameObsolete=false"
+            "SELECT MemberName, MemberType, RevisionNum FROM SourceMember WHERE IsNameObsolete=false"
         )
         return changes
+
+    def _write_retrieve_status(self):
+        with open(self._retrieve_status_path, "w") as f:
+            f.write(json.dumps(self._retrieve_status))
 
     def _run_task(self):
         changes = self._get_changes()
@@ -60,6 +88,10 @@ class ListChanges(BaseSalesforceApiTask):
                 "Ignored {} changed components in the scratch org:".format(ignored)
             )
 
+        if self.options["snapshot"]:
+            self.logger.info("Storing snapshot of changes")
+            self._write_retrieve_status()
+
     def _filter_changes(self, changes):
         filtered = []
         for change in changes["records"]:
@@ -72,7 +104,13 @@ class ListChanges(BaseSalesforceApiTask):
                 continue
             if any(re.search(s, full_name) for s in self._exclude):
                 continue
+            revnum = self._retrieve_status.get(mdtype, {}).get(name)
+            if revnum and revnum == change["RevisionNum"]:
+                continue
             filtered.append(change)
+
+            self._retrieve_status.setdefault(mdtype, {})[name] = change["RevisionNum"]
+
         return filtered
 
 
@@ -95,6 +133,7 @@ class RetrieveChanges(BaseRetrieveMetadata, ListChanges, BaseSalesforceApiTask):
     task_options = retrieve_changes_task_options
 
     def _init_options(self, kwargs):
+        kwargs["snapshot"] = True
         super(RetrieveChanges, self)._init_options(kwargs)
 
         if "api_version" not in self.options:
@@ -113,8 +152,12 @@ class RetrieveChanges(BaseRetrieveMetadata, ListChanges, BaseSalesforceApiTask):
         if not filtered:
             self.logger.info("No changes to retrieve")
             return
+
         for change in filtered:
             type_members[change["MemberType"]].append(change["MemberName"])
+            self.retrieve_status.get(change["MemberType"], {})[
+                change["MemberName"]
+            ] = change["RevisionNum"]
             self.logger.info("{MemberType}: {MemberName}".format(**change))
 
         package_xml_path = os.path.join(self.options["path"], "package.xml")
@@ -150,7 +193,6 @@ class RetrieveChanges(BaseRetrieveMetadata, ListChanges, BaseSalesforceApiTask):
         return self.api_class(self, package_xml, self.options.get("api_version"))
 
     def _run_task(self):
-        changes = self._get_changes()
         super(RetrieveChanges, self)._run_task()
 
         # update package.xml
