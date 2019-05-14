a8a7cca8535588b6c613c3de28381e7471cf20aa
Bryan Oakley <bryan.oakley@gmail.com>
2019-05-14 09:49:03 -0500

    pageType=>page_type to be consistent with other page object code. Thanks for catching this in a code review, David!

--- cumulusci/robotframework/pageobjects/__init__.py
+++ cumulusci/robotframework/pageobjects/__init__.py
@@ -3,12 +3,12 @@ from .PageObjects import PageObjects  # noqa: F401
 from .baseobjects import BasePage, ListingPage, HomePage, DetailPage  # noqa: F401
 
 
-def pageobject(pageType, object_name=None):
+def pageobject(page_type, object_name=None):
     """A decorator to designate a class as a page object"""
-    BuiltIn().log("importing page object {} {}".format(pageType, object_name), "DEBUG")
+    BuiltIn().log("importing page object {} {}".format(page_type, object_name), "DEBUG")
 
     def wrapper(cls):
-        key = (pageType, object_name)
+        key = (page_type, object_name)
         PageObjects.registry[key] = cls
         if not hasattr(cls, "object_name"):
             cls.object_name = object_name
