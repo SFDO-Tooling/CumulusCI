06bf409c6afe65d7e930ddb2c68152660fbdb8b5
Paul Prescod <pprescod@salesforce.com>
2019-09-02 16:25:40 -0700

    Test cases for new classes and utilities.

---
+++ cumulusci/tasks/bulkdata/tests/test_base_generate_data_tasks.py
@@ -0,0 +1,45 @@
+import os
+import unittest
+
+from sqlalchemy import Unicode
+
+from cumulusci.tasks.bulkdata.base_generate_data_task import BaseGenerateDataTask
+from cumulusci.tasks.bulkdata.tests.test_bulkdata import _make_task
+from cumulusci.utils import temporary_dir
+
+
+NUM_RECORDS = 20
+
+
+class DummyBaseBatchDataTask(BaseGenerateDataTask):
+    def generate_data(self, session, engine, base, num_records):
+        assert os.path.exists(self._testfilename)
+        assert session
+        assert engine
+        assert base.classes["households"]
+        assert base.classes["contacts"]
+        t = base.classes["contacts"]
+        assert isinstance(t.email.type, Unicode)
+
+        assert num_records == NUM_RECORDS
+
+
+class TestBaseBatchDataTask(unittest.TestCase):
+    def test_BaseBatchDataTask(self):
+        mapping_file = os.path.join(os.path.dirname(__file__), "mapping_v2.yml")
+        with temporary_dir() as d:
+            tmp_db_path = os.path.join(d, "temp.db")
+            dburl = "sqlite:///" + tmp_db_path
+
+            task = _make_task(
+                DummyBaseBatchDataTask,
+                {
+                    "options": {
+                        "num_records": NUM_RECORDS,
+                        "mapping": mapping_file,
+                        "database_url": dburl,
+                    }
+                },
+            )
+            task._testfilename = tmp_db_path
+            task()
