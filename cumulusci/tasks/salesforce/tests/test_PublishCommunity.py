6460a7779fdb32f446f28253fe12bd75864334e3
Matthew Blanski <mblanski@salesforce.com>
2019-09-12 16:02:51 -0500

    Added publish community test

---
+++ cumulusci/tasks/salesforce/tests/test_PublishCommunity.py
@@ -0,0 +1,66 @@
+import responses
+import unittest
+from cumulusci.tasks.salesforce import PublishCommunity
+from .util import create_task
+
+
+task_options = {"communityid": "000000000000000000"}
+
+
+class test_PublishCommunity(unittest.TestCase):
+    @responses.activate
+    def test_publishes_community(self):
+        cc_task = create_task(PublishCommunity, task_options)
+        community_url = "{}/services/data/v46.0/connect/communities/{}".format(
+            cc_task.org_config.instance_url, task_options["communityid"]
+        )
+        community_publish_url = "{}/services/data/v46.0/connect/communities/{}/publish".format(
+            cc_task.org_config.instance_url, task_options["communityid"]
+        )
+
+        responses.add(
+            method=responses.GET,
+            url=community_url,
+            status=200,
+            json={
+                "allowChatter​AccessWithoutLogin": "false",
+                "allowMembers​ToFlag": "false",
+                "description": "This is a test community",
+                "id": "{}".format(task_options["communityid"]),
+                "invitations​Enabled": "false",
+                "knowledgeable​Enabled": "false",
+                "loginUrl": "https://mydomain.force.com/test/s/login",
+                "memberVisibilityEnabled": "true",
+                "name": "Test Community",
+                "nicknameDisplayEnabled": "false",
+                "privateMessagesEnabled": "false",
+                "reputationEnabled": "false",
+                "sendWelcomeEmail": "true",
+                "siteAsContainerEnabled": "true",
+                "siteUrl": "https://mydomain.force.com/test",
+                "status": "Live",
+                "templateName": "VF Template",
+                "url": "/services/data/v46.0/connect/communities/{}".format(
+                    task_options["communityid"]
+                ),
+                "urlPathPrefix": "test",
+            },
+        )
+
+        responses.add(
+            method=responses.POST,
+            url=community_publish_url,
+            status=200,
+            json={
+                "id": "{}".format(task_options["communityid"]),
+                "message": "We are publishing your changes now. You will receive an email confirmation when your changes are live.",
+                "name": "Test Community",
+                "url": "https://mydomain.force.com/test",
+            },
+        )
+
+        cc_task()
+
+        self.assertEqual(2, len(responses.calls))
+        self.assertEqual(community_url, responses.calls[0].request.url)
+        self.assertEqual(community_publish_url, responses.calls[1].request.url)
