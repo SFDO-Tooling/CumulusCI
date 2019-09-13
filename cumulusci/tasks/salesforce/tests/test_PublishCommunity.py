9b43087cb028b1c8435c35a96bdfd91657489c52
Matthew Blanski <mblanski@salesforce.com>
2019-09-13 15:28:31 -0500

    Updated publish community test for better function

--- cumulusci/tasks/salesforce/tests/test_PublishCommunity.py
+++ cumulusci/tasks/salesforce/tests/test_PublishCommunity.py
@@ -1,6 +1,7 @@
 import responses
 import unittest
 from cumulusci.tasks.salesforce import PublishCommunity
+from cumulusci.core.exceptions import SalesforceException
 from .util import create_task
 
 
@@ -24,7 +25,7 @@ class test_PublishCommunity(unittest.TestCase):
             url=communities_url,
             status=200,
             json={
-                "communities:": [
+                "communities": [
                     {
                         "allowChatter​AccessWithoutLogin": "false",
                         "allowMembers​ToFlag": "false",
@@ -48,7 +49,8 @@ class test_PublishCommunity(unittest.TestCase):
                         ),
                         "urlPathPrefix": "test",
                     }
-                ]
+                ],
+                "total": "1",
             },
         )
 
@@ -126,3 +128,108 @@ class test_PublishCommunity(unittest.TestCase):
         self.assertEqual(2, len(responses.calls))
         self.assertEqual(community_url, responses.calls[0].request.url)
         self.assertEqual(community_publish_url, responses.calls[1].request.url)
+
+    @responses.activate
+    def test_throws_exception_for_bad_name(self):
+        cc_task = create_task(PublishCommunity, task_options)
+        communities_url = "{}/services/data/v46.0/connect/communities".format(
+            cc_task.org_config.instance_url
+        )
+
+        responses.add(
+            method=responses.GET,
+            url=communities_url,
+            status=200,
+            json={
+                "communities": [
+                    {
+                        "allowChatter​AccessWithoutLogin": "false",
+                        "allowMembers​ToFlag": "false",
+                        "description": "This is a test community",
+                        "id": "{}".format(task_options_with_id["communityid"]),
+                        "invitations​Enabled": "false",
+                        "knowledgeable​Enabled": "false",
+                        "loginUrl": "https://mydomain.force.com/test/s/login",
+                        "memberVisibilityEnabled": "true",
+                        "name": "Not {}".format(task_options["name"]),
+                        "nicknameDisplayEnabled": "false",
+                        "privateMessagesEnabled": "false",
+                        "reputationEnabled": "false",
+                        "sendWelcomeEmail": "true",
+                        "siteAsContainerEnabled": "true",
+                        "siteUrl": "https://mydomain.force.com/test",
+                        "status": "Live",
+                        "templateName": "VF Template",
+                        "url": "/services/data/v46.0/connect/communities/{}".format(
+                            task_options_with_id["communityid"]
+                        ),
+                        "urlPathPrefix": "test",
+                    }
+                ],
+                "total": "1",
+            },
+        )
+
+        with self.assertRaises(SalesforceException):
+            cc_task._run_task()
+
+    @responses.activate
+    def test_throws_exception_for_bad_id(self):
+        cc_task = create_task(PublishCommunity, task_options_with_id)
+        community_url = "{}/services/data/v46.0/connect/communities/{}".format(
+            cc_task.org_config.instance_url, task_options_with_id["communityid"]
+        )
+
+        responses.add(
+            method=responses.GET,
+            url=community_url,
+            status=404,
+            json=[
+                {
+                    "errorCode": "NOT_FOUND",
+                    "message": "The requested resource does not exist",
+                }
+            ],
+        )
+
+        with self.assertRaises(SalesforceException):
+            cc_task._run_task()
+
+    @responses.activate
+    def test_throws_exeption_for_unmatched_id(self):
+        cc_task = create_task(PublishCommunity, task_options_with_id)
+        community_url = "{}/services/data/v46.0/connect/communities/{}".format(
+            cc_task.org_config.instance_url, task_options_with_id["communityid"]
+        )
+
+        responses.add(
+            method=responses.GET,
+            url=community_url,
+            status=200,
+            json={
+                "allowChatter​AccessWithoutLogin": "false",
+                "allowMembers​ToFlag": "false",
+                "description": "This is a test community",
+                "id": "{}".format(task_options_with_id["communityid"]),
+                "invitations​Enabled": "false",
+                "knowledgeable​Enabled": "false",
+                "loginUrl": "https://mydomain.force.com/test/s/login",
+                "memberVisibilityEnabled": "true",
+                "name": "Not {}".format(task_options["name"]),
+                "nicknameDisplayEnabled": "false",
+                "privateMessagesEnabled": "false",
+                "reputationEnabled": "false",
+                "sendWelcomeEmail": "true",
+                "siteAsContainerEnabled": "true",
+                "siteUrl": "https://mydomain.force.com/test",
+                "status": "Live",
+                "templateName": "VF Template",
+                "url": "/services/data/v46.0/connect/communities/{}".format(
+                    task_options_with_id["communityid"]
+                ),
+                "urlPathPrefix": "test",
+            },
+        )
+
+        with self.assertRaises(SalesforceException):
+            cc_task._run_task()
