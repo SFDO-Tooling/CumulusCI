90d46ff400f8c38ab6f70ac2c5420d889ee23b5f
Paul Prescod <pprescod@salesforce.com>
2019-09-23 11:27:17 -0700

    Add docstrings.

--- cumulusci/robotframework/salesforce_robot_library_base.py
+++ cumulusci/robotframework/salesforce_robot_library_base.py
@@ -8,6 +8,9 @@ from cumulusci.tasks.apex.batch import BatchApexWait
 
 
 class SalesforceRobotLibraryBase(object):
+    """Base class for any Robot Keyword Library which needs access to
+       Salesforce and CumulusCI features."""
+
     logger = logging.getLogger("robot_debug")
     logger.setLevel("DEBUG")
 
@@ -17,18 +20,23 @@ class SalesforceRobotLibraryBase(object):
 
     @property
     def cumulusci(self):
+        """Get access to the CumulusCI Keyword Library"""
         return self.builtin.get_library_instance("cumulusci.robotframework.CumulusCI")
 
     @property
     def salesforce(self):
+        """Get access to the Salesforce Keyword Library"""
         return self.builtin.get_library_instance("cumulusci.robotframework.Salesforce")
 
     def _run_subtask(self, taskclass, **options):
+        """Helper method for running CCI tasks"""
         subtask_config = TaskConfig({"options": options})
         return self.cumulusci._run_task(taskclass, subtask_config)
 
     def _batch_apex_wait(self, class_name):
+        """Helper method for waiting for Salesforce jobs to end"""
         return self._run_subtask(BatchApexWait, class_name=class_name)
 
     def _run_apex(self, code):
+        """Helper method for running Apex code easily"""
         return self._run_subtask(AnonymousApexTask, apex=code)
