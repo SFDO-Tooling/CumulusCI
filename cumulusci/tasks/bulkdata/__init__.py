643526de879e2e10c48949bccf34dc09850e88f5
Paul Prescod <pprescod@salesforce.com>
2019-10-08 02:46:54 -0700

    Minor cleanup.

--- cumulusci/tasks/bulkdata/__init__.py
+++ cumulusci/tasks/bulkdata/__init__.py
@@ -1,10 +1,8 @@
-from cumulusci.tasks.bulkdata.delete import DeleteData  # noqa: F401
+from cumulusci.tasks.bulkdata.delete import DeleteData
 from cumulusci.tasks.bulkdata.extract import ExtractData
-from cumulusci.tasks.bulkdata.generate import GenerateMapping  # noqa: F401
-from cumulusci.tasks.bulkdata.load import LoadData  # noqa: F401
-from cumulusci.tasks.bulkdata.generate_and_load_data import (
-    GenerateAndLoadData,
-)  # noqa: F401
+from cumulusci.tasks.bulkdata.generate import GenerateMapping
+from cumulusci.tasks.bulkdata.load import LoadData
+from cumulusci.tasks.bulkdata.generate_and_load_data import GenerateAndLoadData
 
 # For backwards-compatibility
 QueryData = ExtractData
