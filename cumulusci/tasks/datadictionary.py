c4e8947f83ad1a111ddf5534a5185e077b05e52f
David Reed <d.reed@salesforce.com>
2019-10-22 18:35:29 -0600

    Complete data dictionary generation

--- cumulusci/tasks/datadictionary.py
+++ cumulusci/tasks/datadictionary.py
@@ -1,8 +1,10 @@
 import csv
 import io
+import os
 import xml.etree.ElementTree as ET
 import zipfile
 
+from collections import defaultdict
 from cumulusci.tasks.github.base import BaseGithubTask
 from distutils.version import LooseVersion
 
@@ -25,7 +27,18 @@ class GenerateDataDictionary(BaseGithubTask):
         },
     }
 
+    def _init_options(self, kwargs):
+        super()._init_options(kwargs)
+
+        self.options["object_path"] = (
+            self.options.get("object_path") or "sObject Data Dictionary.csv"
+        )
+        self.options["field_path"] = (
+            self.options.get("field_path") or "Field Data Dictionary.csv"
+        )
+
     def _run_task(self):
+        self.logger.info("Starting data dictionary generation")
         self.schema = defaultdict(
             lambda: {"version": None, "fields": defaultdict(lambda: {"version": None})}
         )
@@ -35,100 +48,174 @@ class GenerateDataDictionary(BaseGithubTask):
 
     def _walk_releases(self):
         repo = self.get_repo()
+
         for release in repo.releases():
             if release.draft or release.prerelease:
                 continue
 
             zip_content = io.BytesIO()
-            repo.archive("zipball", zip_content, ref=release.ref)
+            repo.archive("zipball", zip_content, ref=release.tag_name)
             zip_file = zipfile.ZipFile(zip_content)
-            version = self._version_from_tag_name(tag_name)
+            version = self._version_from_tag_name(release.tag_name)
 
-            path = zipfile.Path(zip_file, "src/objects")
-            if path.exists():
+            self.logger.info(f"Analyzing version {version}")
+
+            # The zip file's manifest starts with a single package component representing
+            # the repo's name, owner, and commit SHA.
+            # Strip that off so we can inspect paths directly.
+            zip_name_list = zip_file.namelist()
+            self.zip_prefix = os.path.normpath(zip_name_list[-1]).split(os.sep)[0]
+            self.name_list = [
+                os.path.join(os.path.sep, *os.path.normpath(p).split(os.sep)[1:]).strip(
+                    os.path.sep
+                )
+                for p in zip_name_list
+            ]
+
+            if "src/objects" in self.name_list:
                 # MDAPI format
-                self._process_mdapi_release(path)
+                self._process_mdapi_release(zip_file, version)
 
-            path = zipfile.Path(zip_file, "force-app/main/default/objects")
-            # FIXME: check sfdx-project.json for directories to process.
-            if path.exists():
+            if "force-app/main/default/objects" in self.name_list:
+                # FIXME: check sfdx-project.json for directories to process.
                 # SFDX format
-                self._process_sfdx_release(path)
+                self._process_sfdx_release(zip_file, version)
 
-    def _process_mdapi_release(self, path):
-        for f in path.listdir():
-            if f.name.endswith(".object"):
-                sobject_name = body[: len(".object")]
+    def _process_mdapi_release(self, zip_file, version):
+        for f in self.name_list:
+            if f.startswith("src/objects") and f.endswith(".object"):
+                sobject_name = os.path.splitext(os.path.split(f)[1])[0]
+                self.logger.debug(f"Processing {sobject_name}")
 
                 self._process_object_element(
-                    sobject_name, ET.fromstring(f.read_text()), version
+                    sobject_name,
+                    ET.fromstring(
+                        zip_file.read(
+                            os.path.join(os.path.sep, self.zip_prefix, f).strip(
+                                os.path.sep
+                            )
+                        )
+                    ),
+                    version,
                 )
 
-    def _process_sfdx_release(self, path):
-        for obj_dir in path.listdir():
-            meta_xml_file = zipfile.Path(
-                zip_file,
-                f"force-app/main/default/objects/{obj_dir}/{obj_dir}.object-meta.xml",
-            )
-            self._process_object_element(
-                obj_dir, ET.fromstring(meta_xml_file.read_text()), version
-            )
-            fields_dir = zipfile.Path(
-                zip_file, f"force-app/main/default/objects/{obj_dir}/fields"
-            )
-            for field_file in fields_dir.listdir():
-                if field_file.name.endswith("field-meta.xml"):
+    def _process_sfdx_release(self, zip_file, version):
+        for obj_file in zip_file.namelist():
+            if obj_file.startswith("/force-app/main/default/objects"):
+                if obj_file.endswith(".object-meta.xml"):
+                    sobject_name = os.path.basename(os.path.split(obj_file)[0])
+                    self.logger.debug(f"Processing {sobject_name}")
+
+                    self._process_object_element(
+                        sobject_name,
+                        ET.fromstring(
+                            zip_file.read(
+                                os.path.join(
+                                    os.path.sep, self.zip_prefix, obj_file
+                                ).strip(os.path.sep)
+                            )
+                        ),
+                        version,
+                    )
+                elif obj_file.endswith(".field-meta.xml"):
+                    sobject_name = os.path.basename(
+                        os.path.split(os.path.split(obj_file)[0])[0]
+                    )
+                    field_name = os.path.basename(os.path.split(obj_file)[0])
+                    self.logger.debug(f"Processing {sobject_name}.{field_name}")
+
                     self._process_field_element(
-                        obj_dir, ET.fromstring(field_file.read_text()), version
+                        sobject_name,
+                        ET.fromstring(
+                            zip_file.read(
+                                os.path.join(
+                                    os.path.sep, self.zip_prefix, obj_file
+                                ).strip(os.path.sep)
+                            )
+                        ),
+                        version,
                     )
 
-    def _process_object_element(self, sobject_name, element, tag_name):
+    def _process_object_element(self, sobject_name, element, version):
         # If this is a custom object, register its presence in this version
-        if "__" in sobject_name:
-            self._set_version_with_props(
-                self.schema[sobject_name],
-                {"version": version, "label": element.find("label").text},
+        if sobject_name.count("__") == 1:
+            help_text_elem = element.find(
+                "{http://soap.sforce.com/2006/04/metadata}description"
             )
 
-            # For MDAPI-format elements. No-op on SFDX.
-            for field in element.findall("fields"):
-                self._process_field_element(sobject_name, field, version)
+            self._set_version_with_props(
+                self.schema[sobject_name],
+                {
+                    "version": version,
+                    "label": element.find(
+                        "{http://soap.sforce.com/2006/04/metadata}label"
+                    ).text,
+                    "help_text": help_text_elem.text if help_text_elem else "",
+                },
+            )
 
-    def _process_field_element(sobject_name, field, version):
+        # For MDAPI-format elements. No-op on SFDX.
+        for field in element.findall("{http://soap.sforce.com/2006/04/metadata}fields"):
+            self._process_field_element(sobject_name, field, version)
+
+    def _process_field_element(self, sobject_name, field, version):
         # `element` may be either a `fields` element (in MDAPI)
         # or a `CustomField` (SFDX)
 
         # If this is a custom field, register its presence in this version
+        field_name = field.find(
+            "{http://soap.sforce.com/2006/04/metadata}fullName"
+        ).text
+        help_text_elem = field.find(
+            "{http://soap.sforce.com/2006/04/metadata}description"
+        )
         if "__" in field_name:
             self._set_version_with_props(
-                self.schema[sobject_name]["fields"][field.find("fullName").text],
+                self.schema[sobject_name]["fields"][field_name],
                 {
                     "version": version,
-                    "help_text": field.find("description").text,
-                    "field_label": field.find("label").text,
+                    "help_text": help_text_elem.text if help_text_elem else "",
+                    "label": field.find(
+                        "{http://soap.sforce.com/2006/04/metadata}label"
+                    ).text,
                 },
             )
 
     def _write_results(self):
-        with open(self.options["object_path"]) as object_file:
+        with open(self.options["object_path"], "w") as object_file:
             writer = csv.writer(object_file)
 
-            writer.writerow(["Object Name", "Object Label", "Version Introduced"])
+            writer.writerow(
+                [
+                    "Object Name",
+                    "Object Label",
+                    "Object Description",
+                    "Version Introduced",
+                ]
+            )
 
             for sobject_name in self.schema:
-                writer.writerow(
-                    [
-                        sobject_name,
-                        self.schema[sobject_name]["label"],
-                        self.schema[sobject_name]["version"],
-                    ]
-                )
+                if sobject_name.count("__") == 1:
+                    writer.writerow(
+                        [
+                            sobject_name,
+                            self.schema[sobject_name]["label"],
+                            self.schema[sobject_name]["help_text"],
+                            self.schema[sobject_name]["version"],
+                        ]
+                    )
 
-        with open(self.options["field_path"]) as field_file:
+        with open(self.options["field_path"], "w") as field_file:
             writer = csv.writer(field_file)
 
             writer.writerow(
-                ["Object Name", "Field Name", "Field Label", "Version Introduced"]
+                [
+                    "Object Name",
+                    "Field Name",
+                    "Field Label",
+                    "Field Help Text",
+                    "Version Introduced",
+                ]
             )
 
             for sobject_name, sobject_data in self.schema.items():
@@ -138,15 +225,28 @@ class GenerateDataDictionary(BaseGithubTask):
                             sobject_name,
                             field_name,
                             field_data["label"],
+                            field_data["help_text"],
                             field_data["version"],
                         ]
                     )
 
     def _version_from_tag_name(self, tag_name):
         return LooseVersion(
-            tag_name[len(self.project_config.project__package__git__prefix_release) :]
+            tag_name[len(self.project_config.project__git__prefix_release) :]
         )
 
     def _set_version_with_props(self, in_dict, props):
-        if in_dict["version"] is None or in_dict["version"] < props["version"]:
-            in_dict.update(props)
+        # We want to persist the oldest known version, but show the most recent
+        # label and help text in the data dictionary.
+        update_props = props.copy()
+
+        if in_dict["version"] is None:
+            pass
+        elif in_dict["version"] < props["version"]:
+            # Update the metadata but not the version.
+            del update_props["version"]
+        elif in_dict["version"] > props["version"]:
+            # Update the version but not the metadata.
+            update_props = {"version": update_props["version"]}
+
+        in_dict.update(update_props)
--- cumulusci/tasks/tests/test_datadictionary.py
+++ cumulusci/tasks/tests/test_datadictionary.py
@@ -4,7 +4,8 @@ from cumulusci.tasks.datadictionary import GenerateDataDictionary
 from cumulusci.tasks.salesforce.util import create_task
 from cumulusci.tests.util import create_project_config
 from distutils.version import LooseVersion
-from unittest.mock import Mock
+from unittest.mock import Mock, mock_open
+import xml.etree.ElementTree as ET
 
 
 class test_GenerateDataDictionary(unittest.TestCase):
@@ -32,3 +33,35 @@ class test_GenerateDataDictionary(unittest.TestCase):
         project_config.project__package__git__prefix_release = "release/"
 
         assert task._version_from_tag_name("release/1.1") == LooseVersion("1.1")
+
+    def test_write_results(self):
+        task = create_task(
+            GenerateDataDictionary,
+            {"object_path": "object.csv", "field_path": "fields.csv"},
+        )
+
+        task.schema = {
+            "Account": {
+                "fields": {"Test__c": {"version": LooseVersion("1.1"), "label": "Test"}}
+            },
+            "Child__c": {
+                "fields": {
+                    "Parent__c": {"version": LooseVersion("1.2"), "label": "Parent"}
+                },
+                "version": LooseVersion("1.0"),
+            },
+        }
+        task._write_results()
+        # FIXME: mock and assert
+
+    def test_process_field_element__new(self):
+        xml_source = """<?xml version="1.0" encoding="UTF-8"?>
+<CustomField xmlns="http://soap.sforce.com/2006/04/metadata">
+    <fullName>Account__c</fullName>
+    <label>Account</label>
+</CustomField>
+"""
+        task._process_field_element("Test__c", ET.fromstring(xml_source), "1.1")
+
+    def test_process_field_element__updated(self):
+        pass
