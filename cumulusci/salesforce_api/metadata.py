ca3989a28dfabbe130f8914f97adc65d4cc9372b
David Glick <dglick@salesforce.com>
2019-10-23 18:51:32 -0400

    disable PyOpenSSL

--- cumulusci/salesforce_api/metadata.py
+++ cumulusci/salesforce_api/metadata.py
@@ -28,6 +28,10 @@ from cumulusci.salesforce_api.exceptions import MetadataComponentFailure
 from cumulusci.salesforce_api.exceptions import MetadataParseError
 from cumulusci.salesforce_api.exceptions import MetadataApiError
 
+from urllib3.contrib import pyopenssl
+
+pyopenssl.extract_from_urllib3()
+
 retry_policy = Retry(backoff_factor=0.3)
 http_adapter = HTTPAdapter(max_retries=retry_policy)
 
