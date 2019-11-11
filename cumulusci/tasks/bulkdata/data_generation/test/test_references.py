6f6ff49013e9926b753572d4ca78e39fc4bd99c8
Paul Prescod <pprescod@salesforce.com>
2019-11-11 13:31:03 -0800

    Test for the reference features.

---
+++ cumulusci/tasks/bulkdata/data_generation/test/test_references.py
@@ -0,0 +1,99 @@
+from io import StringIO
+import unittest
+import mock
+
+from cumulusci.tasks.bulkdata.data_generation.generate_from_yaml import _generate
+from cumulusci.tasks.bulkdata.data_generation.test.test_parse_samples import find_row
+
+simple_parent = """                     #1
+- object: A                             #2
+  fields:                               #3
+    B:                                  #4
+        object: B                       #5
+        fields:                         #6
+            A_ref:                      #7
+                reference: A            #8
+    """
+
+simple_parent_list = """                #1
+- object: A                             #2
+  fields:                               #3
+    B:                                  #4
+      - object: B                       #5
+        fields:                         #6
+            A_ref:                      #7
+                reference: A            #8
+    """
+
+
+ancestor_reference = """                #1
+- object: A                             #2
+  fields:                               #3
+    B:                                  #4
+      - object: B                       #5
+        fields:                         #6
+            C:                          #7
+               object: C                #8
+               fields:                  #9
+                  A_ref:                #10
+                     reference: A       #11
+    """
+
+reference_from_friend = """             #1
+- object: A                             #2
+  friends:                              #3
+      - object: B                       #4
+        fields:                         #5
+            A_ref:                      #6
+                reference: A            #7
+    """
+
+write_row_path = "cumulusci.tasks.bulkdata.data_generation.output_streams.DebugOutputStream.write_row"
+
+
+class TestReferences(unittest.TestCase):
+    @mock.patch(write_row_path)
+    def test_simple_parent(self, write_row):
+        _generate(StringIO(simple_parent), 1, {}, None, None)
+
+        _, a_values = find_row("A", {}, write_row.mock_calls).args
+        _, b_values = find_row("B", {}, write_row.mock_calls).args
+        id_a = a_values["id"]
+        reference_b = a_values["B"]
+        id_b = b_values["id"]
+        reference_a = b_values["A_ref"]
+        assert id_a == reference_a
+        assert id_b == reference_b
+
+    @mock.patch(write_row_path)
+    def test_simple_parent_list_child(self, write_row):
+        _generate(StringIO(simple_parent_list), 1, {}, None, None)
+
+        _, a_values = find_row("A", {}, write_row.mock_calls).args
+        _, b_values = find_row("B", {}, write_row.mock_calls).args
+        id_a = a_values["id"]
+        reference_b = a_values["B"]
+        id_b = b_values["id"]
+        reference_a = b_values["A_ref"]
+        assert id_a == reference_a
+        assert id_b == reference_b
+
+    @mock.patch(write_row_path)
+    def test_ancestor_reference(self, write_row):
+        _generate(StringIO(ancestor_reference), 1, {}, None, None)
+
+        _, a_values = find_row("A", {}, write_row.mock_calls).args
+        _, c_values = find_row("C", {}, write_row.mock_calls).args
+        id_a = a_values["id"]
+        reference_a = c_values["A_ref"]
+        assert id_a == reference_a
+
+    @mock.patch(write_row_path)
+    def test_reference_from_friend(self, write_row):
+        _generate(StringIO(reference_from_friend), 1, {}, None, None)
+
+        _, a_values = find_row("A", {}, write_row.mock_calls).args
+        _, b_values = find_row("B", {}, write_row.mock_calls).args
+        id_a = a_values["id"]
+        reference_a = b_values["A_ref"]
+        assert id_a == reference_a
