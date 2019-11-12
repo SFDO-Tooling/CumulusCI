e9344c43bfed44375ed65f897121740836f79b86
Paul Prescod <pprescod@salesforce.com>
2019-11-12 13:58:29 -0800

    1. Rename file. 2. Slightly improve informative output.

--- cumulusci/tasks/bulkdata/__init__.py
+++ cumulusci/tasks/bulkdata/__init__.py
@@ -1,6 +1,6 @@
 from cumulusci.tasks.bulkdata.delete import DeleteData
 from cumulusci.tasks.bulkdata.extract import ExtractData
-from cumulusci.tasks.bulkdata.generate import GenerateMapping
+from cumulusci.tasks.bulkdata.generate_mapping import GenerateMapping
 from cumulusci.tasks.bulkdata.load import LoadData
 from cumulusci.tasks.bulkdata.generate_and_load_data import GenerateAndLoadData
 
