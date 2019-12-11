7945b5acdebf94a6b38b3c2cd6e035590a33815c
Paul Prescod <pprescod@salesforce.com>
2019-12-10 21:36:35 -0800

    More sophisticated RemoveElements task.

--- cumulusci/tasks/metadata/tests/test_modify.py
+++ cumulusci/tasks/metadata/tests/test_modify.py
@@ -9,27 +9,82 @@ from cumulusci.utils import temporary_dir
 
 
 class TestRemoveElementsXPath(unittest.TestCase):
-    def test_run_task(self):
+    def run_xml_through_task(self, xml, options):
         with temporary_dir() as path:
             xml_path = os.path.join(path, "test.xml")
             with open(xml_path, "w") as f:
-                f.write("<root><todelete /></root>")
+                f.write(xml)
 
             project_config = BaseProjectConfig(
                 BaseGlobalConfig(), config={"noyaml": True}
             )
-            task_config = TaskConfig(
-                {
-                    "options": {
-                        "elements": [{"path": "test.xml", "xpath": "./todelete"}],
-                        "chdir": path,
-                    }
-                }
-            )
+            task_config = TaskConfig({"options": {**options, "chdir": path}})
             task = RemoveElementsXPath(project_config, task_config)
             task()
             with open(xml_path, "r") as f:
                 result = f.read()
-            self.assertEqual(
-                '<?xml version="1.0" encoding="UTF-8"?>\n<root/>\n', result
-            )
+            return result
+
+    def test_run_task(self):
+        result = self.run_xml_through_task(
+            "<root><todelete/></root>",
+            {"elements": [{"path": "test.xml", "xpath": "./todelete"}]},
+        )
+        self.assertEqual('<?xml version="1.0" encoding="UTF-8"?>\n<root/>\n', result)
+
+    def test_salesforce_encoding(self):
+        result = self.run_xml_through_task(
+            """<root><todelete /><a>"'</a></root>""",
+            {
+                "elements": [{"path": "test.xml", "xpath": "todelete"}],
+                "output_style": "salesforce",
+            },
+        )
+        self.assertEqual(
+            '<?xml version="1.0" encoding="UTF-8"?>\n<root xmlns="http://soap.sforce.com/2006/04/metadata"><a>&quot;&apos;</a>\n</root>\n',
+            result,
+        )
+
+    def test_namespaces_ns(self):
+        result = self.run_xml_through_task(
+            """<root xmlns="http://soap.sforce.com/2006/04/metadata"><todelete /><a>"'</a></root>""",
+            {
+                "elements": [{"path": "test.xml", "xpath": "ns:todelete"}],
+                "output_style": "salesforce",
+            },
+        )
+        self.assertEqual(
+            '<?xml version="1.0" encoding="UTF-8"?>\n<root xmlns="http://soap.sforce.com/2006/04/metadata"><a>&quot;&apos;</a>\n</root>\n',
+            result,
+        )
+
+    def test_regular_expressions(self):
+        result = self.run_xml_through_task(
+            """<root xmlns="http://soap.sforce.com/2006/04/metadata"><todelete>baz</todelete><todelete>bar</todelete></root>""",
+            {
+                "elements": [
+                    {
+                        "path": "test.xml",
+                        "xpath": "ns:todelete[re:match('bar', text())]",
+                    }
+                ],
+                "output_style": "salesforce",
+            },
+        )
+        self.assertEqual(
+            '<?xml version="1.0" encoding="UTF-8"?>\n<root xmlns="http://soap.sforce.com/2006/04/metadata"><todelete>baz</todelete>\n</root>\n',
+            result,
+        )
+
+    def test_comment(self):
+        result = self.run_xml_through_task(
+            """<root><a><!-- Foo --></a><todelete></todelete></root>""",
+            {
+                "elements": [{"path": "test.xml", "xpath": "todelete"}],
+                "output_style": "salesforce",
+            },
+        )
+        self.assertEqual(
+            '<?xml version="1.0" encoding="UTF-8"?>\n<root xmlns="http://soap.sforce.com/2006/04/metadata"><a><!-- Foo --></a>\n</root>\n',
+            result,
+        )
