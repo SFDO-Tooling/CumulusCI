59c6aefc470beb447a2684b4179ad86fbc9f2963
jofsky <josh.kofsky@gmail.com>
2019-12-10 22:25:13 -0800

    updating vacuously true test case need to review

--- cumulusci/tasks/salesforce/tests/test_ActivateProcessBuilderProcesses.py
+++ cumulusci/tasks/salesforce/tests/test_ActivateProcessBuilderProcesses.py
@@ -3,17 +3,18 @@ import unittest
 
 from cumulusci.core.config import BaseGlobalConfig
 from cumulusci.core.config import BaseProjectConfig
-from cumulusci.tasks.salesforce import CreatePackage
+
+# from cumulusci.tasks.salesforce import ActivateFlowProcesses
 from .util import create_task
 
 
-class TestCreatePackage(unittest.TestCase):
-    @mock.patch("cumulusci.salesforce_api.package_zip.CreatePackageZipBuilder.__call__")
-    def test_get_package_zip(self, CreatePackageZipBuilder):
+class TestActivateFlowProcesses(unittest.TestCase):
+    @mock.patch("cumulusci.tasks.salesforce.ActivateFlowProcesses")
+    def test_activate_flow_processes(self, ActivateFlowProcesses):
         project_config = BaseProjectConfig(
             BaseGlobalConfig(),
             {"project": {"package": {"name": "TestPackage", "api_version": "43.0"}}},
         )
-        task = create_task(CreatePackage, project_config=project_config)
-        task._get_package_zip()
-        CreatePackageZipBuilder.assert_called_once()
+        task = create_task(ActivateFlowProcesses, project_config=project_config)
+        task._get_ActivateFlowProcesses()
+        ActivateFlowProcesses.assert_called()
