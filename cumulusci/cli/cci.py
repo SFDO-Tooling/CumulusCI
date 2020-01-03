7397063d948d71f9501cfac3bd9106fd9acbdd03
Brandon Parker <brandon.parker@salesforce.com>
2020-01-03 10:23:53 -0800

    Update cumulusci/cli/cci.py
    Co-Authored-By: David Glick <dglick@salesforce.com>

--- cumulusci/cli/cci.py
+++ cumulusci/cli/cci.py
@@ -166,7 +166,7 @@ def pass_runtime(func=None, require_project=True):
 
 
 SUGGEST_GIT_GIST_COMMAND = """\nIt looks like you may have run into error. Did you know cci has a command for sending this error to a GitHub gist?
-Just run `$ cci gist` and make sure that your GitHub access token as the 'create gist' scope.\n"""
+Just run `$ cci gist` and make sure that your GitHub access token has the 'create gist' scope.\n"""
 
 
 #
