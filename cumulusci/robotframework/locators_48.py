23585e63c8f9d001d4acb2d46e0b6ef6d015c5f5
Bryan Oakley <bryan.oakley@gmail.com>
2020-01-14 10:10:27 -0600

    Some Spring '20 locator changes

--- cumulusci/robotframework/locators_48.py
+++ cumulusci/robotframework/locators_48.py
@@ -2,4 +2,16 @@ from cumulusci.robotframework import locators_47
 
 lex_locators = locators_47.lex_locators.copy()
 
-# At the moment, there are no changes to the locators.
+lex_locators["record"]["header"]["field_value"] = (
+    "//records-lwc-highlights-panel"
+    "//force-highlights-details-item"
+    "[.//*[contains(@class, 'slds-text-title') and text()='{}']]"
+    "//p[contains(@class, 'fieldComponent')]//*[text()]"
+)
+
+lex_locators["record"]["header"]["field_value_link"] = (
+    "//records-lwc-highlights-panel"
+    "//force-highlights-details-item"
+    "[.//*[contains(@class, 'slds-text-title') and text()='{}']]"
+    "//p[contains(@class, 'fieldComponent')]//a[text()]"
+)
