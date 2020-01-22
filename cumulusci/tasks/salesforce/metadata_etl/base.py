ba9a6ee4dd3d17fdd60b9e05ffc937eab5033dba
David Reed <d.reed@salesforce.com>
2020-01-22 12:33:05 -0700

    Initial Metadata ETL sketch

---
+++ cumulusci/tasks/salesforce/metadata_etl/base.py
@@ -0,0 +1,170 @@
+import tempfile
+
+from pathlib import Path
+
+from cumulusci.core.exceptions import CumulusCIException
+from cumulusci.tasks.salesforce import BaseSalesforceApiTask
+from cumulusci.tasks.metadata import ApiRetrieveUnpackaged
+from cumulusci.tasks.metadata.package import PackageXmlGenerator
+from cumulusci.utils import elementtree_parse_file
+
+
+class BaseMetadataETLTask(BaseSalesforceApiTask):
+    deploy = False
+    retrieve = False
+
+    def _generate_package_xml(self):
+        return f"""<?xml version="1.0" encoding="UTF-8"?>
+        <Package xmlns="http://soap.sforce.com/2006/04/metadata">
+            <version>{self.api_version}</version>
+        </Package>
+        """
+
+    def _create_directories(self, tempdir):
+        if self.retrieve:
+            self.retrieve_dir = Path(tempdir, "retrieve")
+            self.retrieve_dir.mkdir()
+        if self.deploy:
+            self.deploy_dir = Path(tempdir, "deploy")
+            self.deploy_dir.mkdir()
+
+    def _retrieve(self):
+        self._package_xml_content = self._generate_package_xml()
+        api_retrieve = ApiRetrieveUnpackaged(
+            self, self._package_xml_content, self.api_version
+        )
+        unpackaged = api_retrieve()
+        unpackaged.extractall(self.retrieve_dir)
+
+    def _run_task(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            self._create_directories(tempdir)
+            if self.retrieve:
+                self._retrieve()
+            self._transform()
+            if self.deploy:
+                self._deploy()
+
+
+class BaseMetadataSynthesisTask(BaseMetadataETLTask):
+    """Base class for Metadata ETL tasks that generate new metadata
+    and deploy it into the org."""
+
+    deploy = True
+
+    def _transform(self):
+        self._synthesize()
+
+    def _synthesize(self):
+        # Create metadata in self.deploy_dir
+        pass
+
+
+class BaseMetadataTransformTask(BaseMetadataETLTask):
+    """Base class for Metadata ETL tasks that extract metadata,
+    transform it, and deploy it back into the org."""
+
+    retrieve = True
+    deploy = True
+
+    def _get_entities(self):
+        return {}
+
+    def _get_types_package_xml(self):
+        base = """<types>
+            {members}
+            <name>{name}</name>
+        </types>
+        """
+        types = ""
+        for entity, api_names in self._get_entities().items():
+            members = "\n".join(
+                f"<members>{api_name}</members>" for api_name in api_names
+            )
+            types += base.format(members=members, name=entity)
+
+        return types
+
+    def _generate_package_xml(self):
+        return f"""<?xml version="1.0" encoding="UTF-8"?>
+        <Package xmlns="http://soap.sforce.com/2006/04/metadata">
+            {self._get_types_package_xml()}
+            <version>{self.api_version}</version>
+        </Package>
+        """
+
+    def _transform(self):
+        pass
+
+    def _deploy(self):
+        generator = PackageXmlGenerator(self.deploy_dir, self.api_version)
+
+        target_profile_xml = Path(self.deploy_dir, "package.xml")
+        target_profile_xml.write_text(generator())
+
+        api = self._get_api(path=self.deploy_dir)
+        return api()
+
+
+class MetadataSingleEntityTransformTask(BaseMetadataTransformTask):
+    """Base class for a Metadata ETL task that affects one or more
+    instances of a specific metadata entity."""
+
+    entity = None
+
+    def __init__(self, api_names):
+        self.api_names = api_names
+
+    def _get_entities(self):
+        return {self.entity: self.api_names or ["*"]}
+
+    def _transform_entity(self, metadata):
+        return metadata
+
+    def _transform(self):
+        # call _transform_entity once per retrieved entity
+        # if the entity is an XML file, provide a parsed version
+        # and write the returned metadata into the deploy directory
+
+        parser = PackageXmlGenerator()  # We'll use it for its metadata_map
+        entity_configurations = list(
+            filter(
+                lambda entry: any(
+                    [
+                        subentry["type"] == self.entity
+                        for subentry in parser.metadata_map[entry]
+                    ]
+                ),
+                parser.metadata_map,
+            )
+        )
+        if not entity_configurations:
+            raise CumulusCIException(
+                f"Unable to locate configuration for entity {self.entity}"
+            )
+
+        configuration = parser.metadata_map[entity_configurations[0]]
+        if configuration["class"] != "MetadataFilenameParser":
+            raise CumulusCIException(
+                f"MetadataSingleEntityTransformTask only supports manipulating complete, file-based XML entities (not {self.entity})"
+            )
+
+        extension = configuration["extension"]
+        directory = entity_configurations[0]
+
+        for api_name in self.api_names:
+            path = self.retrieve_dir / directory / f"{api_name}.{extension}"
+            if not path.exists():
+                raise CumulusCIException(f"Cannot find metadata file {path}")
+
+            transformed_xml = self._transform_entity(elementtree_parse_file(path))
+            if transformed_xml:
+                parent_dir = self.deploy_dir / directory
+                parent_dir.mkdir()
+                destination_path = parent_dir / f"{api_name}.{extension}"
+                transformed_xml.write(
+                    destination_path,
+                    "utf-8",
+                    xml_declaration=True,
+                    default_namespace=self.namespaces["sf"],
+                )
