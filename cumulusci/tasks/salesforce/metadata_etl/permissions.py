d6104a8e9e14ec12efdf5a78d89e61b2829739bf
David Reed <d.reed@salesforce.com>
2020-01-29 08:20:26 -0700

    Add AddPermissions and tests for same

---
+++ cumulusci/tasks/salesforce/metadata_etl/permissions.py
@@ -0,0 +1,124 @@
+import xml.etree.ElementTree as XML_ET
+
+from cumulusci.core.exceptions import TaskOptionsError
+from cumulusci.tasks.salesforce.metadata_etl import (
+    MetadataSingleEntityTransformTask,
+    get_new_tag_index,
+)
+
+
+class AddPermissions(MetadataSingleEntityTransformTask):
+    entity = "PermissionSet"
+    task_options = {
+        "field_permissions": {
+            "description": "Array of fieldPermissions objects to upsert into permission_set.  Each fieldPermission requires the following attributes: 'field': API Name of the field including namespace; 'readable': boolean if field can be read; 'editable': boolean if field can be edited",
+            "required": "False",
+        },
+        "class_accesses": {
+            "description": "Array of classAccesses objects to upsert into permission_set.  Each classAccess requires the following attributes: 'apexClass': Name of Apex Class.  If namespaced, make sure to use the form \"namespace__ApexClass\"; 'enabled': boolean if the Apex Class can be accessed.",
+            "required": "False",
+        },
+        **MetadataSingleEntityTransformTask.task_options,
+    }
+
+    def _transform_entity(self, metadata, api_name):
+        self._upsert_class_accesses(metadata, api_name)
+        self._upsert_field_permissions(metadata, api_name)
+
+        return metadata
+
+    def _upsert_class_accesses(self, metadata, api_name):
+        class_accesses = self.options.get("class_accesses")
+
+        if not class_accesses:
+            return
+
+        self.logger.info(f"Upserting class accesses for {api_name}")
+
+        new_permission_index = get_new_tag_index(
+            metadata, "classAccesses", self.namespaces
+        )
+
+        for class_access in class_accesses:
+            if "apexClass" not in class_access:
+                raise TaskOptionsError(
+                    "class_access entries must contain the 'apexClass' key."
+                )
+
+            existing_permissions = metadata.findall(
+                f".//sf:classAccesses[sf:apexClass='{class_access['apexClass']}']",
+                self.namespaces,
+            )
+            if 0 < len(existing_permissions):
+                # Permission exists: update
+                for elem in existing_permissions:
+                    elem.find("sf:enabled", self.namespaces).text = str(
+                        class_access.get("enabled", True)
+                    ).lower()
+            else:
+                # Permission doesn't exist: insert
+                elem = XML_ET.Element("{%s}classAccesses" % (self.namespaces.get("sf")))
+                metadata.getroot().insert(new_permission_index, elem)
+
+                elem_apexClass = XML_ET.SubElement(
+                    elem, "{%s}apexClass" % (self.namespaces.get("sf"))
+                )
+                elem_apexClass.text = class_access.get("apexClass")
+
+                elem_enabled = XML_ET.SubElement(
+                    elem, "{%s}enabled" % (self.namespaces.get("sf"))
+                )
+                elem_enabled.text = str(class_access.get("enabled", True)).lower()
+
+    def _upsert_field_permissions(self, metadata, api_name):
+        field_permissions = self.options.get("field_permissions")
+
+        if not field_permissions:
+            return
+
+        self.logger.info(f"Upserting Field Level Security for {api_name}")
+
+        new_permission_index = get_new_tag_index(
+            metadata, "fieldPermissions", self.namespaces
+        )
+
+        for field_permission in field_permissions:
+            if "field" not in field_permission:
+                raise TaskOptionsError(
+                    "field_permissions entries must include the 'field' key."
+                )
+
+            existing_permissions = metadata.findall(
+                f".//sf:fieldPermissions[sf:field='{field_permission['field']}']",
+                self.namespaces,
+            )
+            if 0 < len(existing_permissions):
+                # Permission exists: update
+                for elem in existing_permissions:
+                    elem.find("sf:readable", self.namespaces).text = str(
+                        field_permission.get("readable", True)
+                    ).lower()
+                    elem.find("sf:editable", self.namespaces).text = str(
+                        field_permission.get("editable", True)
+                    ).lower()
+            else:
+                # Permission doesn't exist: insert
+                elem = XML_ET.Element(
+                    "{%s}fieldPermissions" % (self.namespaces.get("sf"))
+                )
+                metadata.getroot().insert(new_permission_index, elem)
+
+                elem_field = XML_ET.SubElement(
+                    elem, "{%s}field" % (self.namespaces.get("sf"))
+                )
+                elem_field.text = field_permission.get("field")
+
+                elem_editable = XML_ET.SubElement(
+                    elem, "{%s}editable" % (self.namespaces.get("sf"))
+                )
+                elem_editable.text = str(field_permission.get("editable", True)).lower()
+
+                elem_readable = XML_ET.SubElement(
+                    elem, "{%s}readable" % (self.namespaces.get("sf"))
+                )
+                elem_readable.text = str(field_permission.get("readable", True)).lower()
---
+++ cumulusci/tasks/salesforce/metadata_etl/tests/test_permissions.py
@@ -0,0 +1,229 @@
+import io
+import unittest
+import xml.etree.ElementTree as ET
+
+from cumulusci.core.exceptions import TaskOptionsError
+from cumulusci.tasks.salesforce.tests.util import create_task
+from cumulusci.tasks.salesforce.metadata_etl import AddPermissions
+
+PERMSET_XML = """<?xml version="1.0" encoding="UTF-8"?>
+<PermissionSet xmlns="http://soap.sforce.com/2006/04/metadata">
+    <applicationVisibilities>
+        <application>CustomApp</application>
+        <visible>false</visible>
+    </applicationVisibilities>
+    <classAccesses>
+        <apexClass>ApexController</apexClass>
+        <enabled>false</enabled>
+    </classAccesses>
+    <fieldPermissions>
+        <editable>false</editable>
+        <field>Test__c.Lookup__c</field>
+        <readable>false</readable>
+    </fieldPermissions>
+    <hasActivationRequired>false</hasActivationRequired>
+    <label>Test</label>
+    <objectPermissions>
+        <allowCreate>false</allowCreate>
+        <allowDelete>false</allowDelete>
+        <allowEdit>false</allowEdit>
+        <allowRead>false</allowRead>
+        <modifyAllRecords>false</modifyAllRecords>
+        <object>Test__c</object>
+        <viewAllRecords>false</viewAllRecords>
+    </objectPermissions>
+    <recordTypeVisibilities>
+        <recordType>Case.Test</recordType>
+        <visible>true</visible>
+    </recordTypeVisibilities>
+    <tabSettings>
+        <tab>standard-report</tab>
+        <visibility>Visible</visibility>
+    </tabSettings>
+    <userPermissions>
+        <enabled>true</enabled>
+        <name>ActivitiesAccess</name>
+    </userPermissions>
+</PermissionSet>
+"""
+
+
+class test_AddPermissions(unittest.TestCase):
+    def test_adds_new_field_permission(self):
+        task = create_task(
+            AddPermissions,
+            {
+                "unmanaged": False,
+                "api_version": "47.0",
+                "api_names": "bar,foo",
+                "field_permissions": [
+                    {
+                        "field": "Test__c.Description__c",
+                        "readable": True,
+                        "editable": True,
+                    }
+                ],
+            },
+        )
+
+        root = ET.ElementTree(file=io.StringIO(PERMSET_XML))
+        namespaces = {"sf": "http://soap.sforce.com/2006/04/metadata"}
+
+        assert (
+            len(
+                root.findall(
+                    f".//sf:fieldPermissions[sf:field='Test__c.Description__c']",
+                    namespaces,
+                )
+            )
+            == 0
+        )
+
+        result = task._transform_entity(root, "PermSet")
+
+        fieldPermissions = result.findall(
+            ".//sf:fieldPermissions[sf:field='Test__c.Description__c']", namespaces
+        )
+        assert len(fieldPermissions) == 1
+        readable = fieldPermissions[0].findall(f".//sf:readable", namespaces)
+        assert len(readable) == 1
+        assert readable[0].text == "true"
+        editable = fieldPermissions[0].findall(f".//sf:editable", namespaces)
+        assert len(editable) == 1
+        assert editable[0].text == "true"
+
+    def test_updates_existing_field_permission(self):
+        task = create_task(
+            AddPermissions,
+            {
+                "unmanaged": False,
+                "api_version": "47.0",
+                "api_names": "bar,foo",
+                "field_permissions": [
+                    {"field": "Test__c.Lookup__c", "readable": True, "editable": True}
+                ],
+            },
+        )
+
+        root = ET.ElementTree(file=io.StringIO(PERMSET_XML))
+        namespaces = {"sf": "http://soap.sforce.com/2006/04/metadata"}
+
+        assert (
+            len(
+                root.findall(
+                    f".//sf:fieldPermissions[sf:field='Test__c.Lookup__c']", namespaces
+                )
+            )
+            == 1
+        )
+
+        result = task._transform_entity(root, "PermSet")
+
+        fieldPermissions = result.findall(
+            ".//sf:fieldPermissions[sf:field='Test__c.Lookup__c']", namespaces
+        )
+        assert len(fieldPermissions) == 1
+        readable = fieldPermissions[0].findall(f".//sf:readable", namespaces)
+        assert len(readable) == 1
+        assert readable[0].text == "true"
+        editable = fieldPermissions[0].findall(f".//sf:editable", namespaces)
+        assert len(editable) == 1
+        assert editable[0].text == "true"
+
+    def test_adds_new_class_permission(self):
+        task = create_task(
+            AddPermissions,
+            {
+                "unmanaged": False,
+                "api_version": "47.0",
+                "api_names": "bar,foo",
+                "class_accesses": [{"apexClass": "LWCController", "enabled": True}],
+            },
+        )
+
+        root = ET.ElementTree(file=io.StringIO(PERMSET_XML))
+        namespaces = {"sf": "http://soap.sforce.com/2006/04/metadata"}
+
+        assert (
+            len(
+                root.findall(
+                    ".//sf:classAccesses[sf:apexClass='LWCController']", namespaces
+                )
+            )
+            == 0
+        )
+
+        result = task._transform_entity(root, "PermSet")
+
+        classAccesses = result.findall(
+            ".//sf:classAccesses[sf:apexClass='LWCController']", namespaces
+        )
+        assert len(classAccesses) == 1
+        enabled = classAccesses[0].findall(f".//sf:enabled", namespaces)
+        assert len(enabled) == 1
+        assert enabled[0].text == "true"
+
+    def test_upserts_existing_class_permission(self):
+        task = create_task(
+            AddPermissions,
+            {
+                "unmanaged": False,
+                "api_version": "47.0",
+                "api_names": "bar,foo",
+                "class_accesses": [{"apexClass": "ApexController", "enabled": True}],
+            },
+        )
+
+        root = ET.ElementTree(file=io.StringIO(PERMSET_XML))
+        namespaces = {"sf": "http://soap.sforce.com/2006/04/metadata"}
+
+        assert (
+            len(
+                root.findall(
+                    ".//sf:classAccesses[sf:apexClass='ApexController']", namespaces
+                )
+            )
+            == 1
+        )
+
+        result = task._transform_entity(root, "PermSet")
+
+        classAccesses = result.findall(
+            ".//sf:classAccesses[sf:apexClass='ApexController']", namespaces
+        )
+        assert len(classAccesses) == 1
+        enabled = classAccesses[0].findall(f".//sf:enabled", namespaces)
+        assert len(enabled) == 1
+        assert enabled[0].text == "true"
+
+    def test_missing_apexclass_throws_exception(self):
+        task = create_task(
+            AddPermissions,
+            {
+                "unmanaged": False,
+                "api_version": "47.0",
+                "api_names": "bar,foo",
+                "class_accesses": [{"enabled": True}],
+            },
+        )
+
+        root = ET.ElementTree(file=io.StringIO(PERMSET_XML))
+
+        with self.assertRaises(TaskOptionsError):
+            task._transform_entity(root, "PermSet")
+
+    def test_missing_field_throws_exception(self):
+        task = create_task(
+            AddPermissions,
+            {
+                "unmanaged": False,
+                "api_version": "47.0",
+                "api_names": "bar,foo",
+                "field_permissions": [{"readable": True}],
+            },
+        )
+
+        root = ET.ElementTree(file=io.StringIO(PERMSET_XML))
+
+        with self.assertRaises(TaskOptionsError):
+            task._transform_entity(root, "PermSet")
