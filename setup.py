7bdfc1453bc729aa5e4929a7f7e1e3513da6a170
David Glick <dglick@salesforce.com>
2020-02-20 00:18:37 -0500

    fix another pypi ref

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("requirements_dev.txt") as dev_requirements_file:
 
 setup(
     name="cumulusci",
-    version="3.6.1.dev6",
+    version="3.6.1.dev7",
     description="Build and release tools for Salesforce developers",
     long_description=readme + u"\n\n" + history,
     long_description_content_type="text/x-rst",
