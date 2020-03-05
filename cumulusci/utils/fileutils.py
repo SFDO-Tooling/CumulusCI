7582a60056179e38e74bdc7263352f49a237b655
Paul Prescod <pprescod@salesforce.com>
2020-03-05 13:21:25 -0800

    Add type declaration.

--- cumulusci/utils/fileutils.py
+++ cumulusci/utils/fileutils.py
@@ -1,4 +1,4 @@
-from typing import IO, ContextManager, Text, Tuple
+from typing import IO, ContextManager, Text, Tuple, Union
 from contextlib import contextmanager
 from pathlib import Path
 from io import TextIOWrapper
@@ -19,7 +19,9 @@ def _get_path_from_stream(stream):
 
 
 @contextmanager
-def load_from_source(source) -> ContextManager[Tuple[Text, IO[Text]]]:
+def load_from_source(
+    source: Union[str, IO, Path]
+) -> ContextManager[Tuple[Text, IO[Text]]]:
     """Normalize potential data sources into uniform tuple
 
      Take as input a file-like, path-like, or URL-like
