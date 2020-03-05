d599001a18913aa02c475bfc3096dfe6d7e1c5a2
Paul Prescod <pprescod@salesforce.com>
2020-03-05 08:17:45 -0800

    Add deprecation warnings

--- cumulusci/tasks/bulkdata/tests/test_mapping_parser.py
+++ cumulusci/tasks/bulkdata/tests/test_mapping_parser.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-
+import logging
+from yaml import safe_load, dump
+from io import StringIO
 from cumulusci.tasks.bulkdata.mapping_parser import parse_mapping_from_yaml
 
 
@@ -19,3 +21,18 @@ class TestMappingParser:
         assert "after" in lookups["ParentId"]
         after_list = {l["after"] for l in lookups.values() if "after" in l}
         assert after_list
+
+    def test_deprecation(self, caplog):
+        base_path = Path(__file__).parent / "mapping_v2.yml"
+        caplog.set_level(logging.WARNING)
+
+        parse_mapping_from_yaml(base_path)
+        assert "record_type" in caplog.text
+
+        with open(base_path) as f:
+            raw_mapping = safe_load(f)
+        print(raw_mapping)
+        raw_mapping["Insert Households"]["oid_as_pk"] = True
+
+        parse_mapping_from_yaml(StringIO(dump(raw_mapping)))
+        assert "oid_as_pk" in caplog.text
