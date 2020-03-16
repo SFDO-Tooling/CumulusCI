da340fed570da4e040631822e10db62b1d9a1267
David Glick <dglick@salesforce.com>
2020-03-16 16:25:25 -0400

    update where build-homebrew.sh gets the version

--- utility/build-homebrew.sh
+++ utility/build-homebrew.sh
@@ -12,7 +12,7 @@ echo " "
 curl -L "https://pypi.io/pypi/cumulusci/json" > "$PYPI_JSON" || exit 1
 PACKAGE_URL="$(cat "$PYPI_JSON" | jq '.urls[1].url')" || exit 1
 PACKAGE_SHA="$(cat "$PYPI_JSON" | jq '.urls[1].digests.sha256')" || exit 1
-PACKAGE_VERSION="$(cat setup.cfg | grep current_version | head -n 1 | cut -f 3 -d' ')" || exit 1
+PACKAGE_VERSION="$(cat cumulusci/version.txt)"
 
 echo " "
 echo "=> Creating a temporary virtualenv and installing CumulusCI..."
