097868d788f229fdc322c6788dd4050c15b0a875
Paul Prescod <pprescod@salesforce.com>
2020-03-26 15:25:53 -0700

    Various improvements.

--- cumulusci/tasks/qar/github.py
+++ cumulusci/tasks/qar/github.py
@@ -1,7 +1,13 @@
-import cumulusci
+from typing import Set
 import datetime
-from jinja2 import Environment, FileSystemLoader
 import os
+import csv
+from itertools import chain
+
+
+from jinja2 import Environment, FileSystemLoader
+
+import cumulusci
 from cumulusci.tasks.github.base import BaseGithubTask
 from cumulusci.core.utils import process_list_arg
 
@@ -22,6 +28,12 @@ class OrganizationReport(BaseGithubTask):
         "repos": {
             "description": "A comma-separated list of Repo names to report for (not including org-name)",
         },
+        "additional_info_csv": {
+            "description": "The path to a CSV file. "
+            "The CSV must have column headings in the first row. "
+            "One of the column headings must be 'Github Username'. "
+            "The other columns will be added to the Member report."
+        },
     }
 
     def _init_options(self, kwargs):
@@ -30,38 +42,83 @@ class OrganizationReport(BaseGithubTask):
             "tasks", "qar", "templates", "github.html",
         )
         repos = self.options.get("repos")
-        self.repos = process_list_arg(repos) if repos else None
+        self.repos = set(process_list_arg(repos)) if repos else None
+        self.additional_info_csv = self.options.get("additional_info_csv")
 
     def _run_task(self):
         org = self.github.organization(self.options["organization"])
+        if self.additional_info_csv:
+            member_extra_info = self._parse_member_extra_info(self.additional_info_csv)
+            extra_fields = list(next(iter(member_extra_info.values())).keys())[1:]
+        else:
+            member_extra_info = {}
+            extra_fields = {}
 
-        org_members = self._fetch_members(org)
-        teams = self._fetch_teams(org)
-        repos = self._fetch_repos(org, org_members, teams)
-        self._report(org, org_members, teams, repos)
+        org_members = self._fetch_members(org, member_extra_info)
+        teams, ignored_teams = self._fetch_teams(org, self.repos)
+        repos, ignored_repos = self._fetch_repos(org, org_members, teams)
+        org_members, ignored_org_members = self._filter_org_members(org_members, repos)
+        ignored = {
+            "teams": ignored_teams,
+            "users": ignored_org_members,
+            "repos": ignored_repos,
+        }
+        self._report(org, org_members, teams, repos, extra_fields, ignored)
 
-    def _fetch_members(self, org):
+    def _filter_org_members(self, org_members, repos):
+        flatten = chain.from_iterable
+
+        relevant_org_members = set(
+            flatten(repo["users"].keys() for repo in repos.values())
+        )
+        rc = {
+            member: org_members[member]
+            for member in org_members.keys()
+            if member in set(relevant_org_members)
+        }
+        ignored = set(org_members) - relevant_org_members
+
+        return rc, ignored
+
+    def _parse_member_extra_info(self, additional_info_csv):
+        with open(additional_info_csv) as f:
+            dict_reader = csv.DictReader(f)
+            return {row["Github Username"]: row for row in dict_reader}
+
+    def _fetch_members(self, org, member_extra_info):
         self.logger.info("Fetching members...")
         org_members = {}
-        for member in org.members(role="admin"):
-            org_members[member.login] = {
+
+        def member_dict(member, admin):
+            extra_info = member_extra_info.get(member.login) or {}
+            return {
                 "obj": member,
-                "admin": True,
+                "admin": admin,
                 "email": self.github.user(member.login).email,
+                **extra_info,
             }
+
+        for member in org.members(role="admin"):
+            org_members[member.login] = member_dict(member, True)
 
         for member in org.members(role="member"):
-            org_members[member.login] = {
-                "obj": member,
-                "admin": False,
-                "email": self.github.user(member.login).email,
-            }
+            org_members[member.login] = member_dict(member, False)
+
         return org_members
 
-    def _fetch_teams(self, org):
+    def _fetch_teams(self, org, relevant_repos: Set[str]):
         teams = {}
+        ignored = set()
         self.logger.info("Fetching teams...")
         for team in org.teams():
+            repos = team.repositories()
+            repo_names = set(repo.name for repo in repos)
+            if relevant_repos and not (set(relevant_repos) & set(repo_names)):
+                self.logger.info(
+                    f"Skipping {team.name} because they do not have access to relevant repos"
+                )
+                ignored.add(team.name)
+                continue
             teams[team.name] = {
                 "obj": team,
                 "members": {},
@@ -75,7 +132,7 @@ class OrganizationReport(BaseGithubTask):
                 teams[team.name]["members"][member.login] = info
             for member in team.members(role="maintainer"):
                 teams[team.name]["members"][member.login]["maintainer"] = True
-            for repo in team.repositories():
+            for repo in repos:
                 info = {
                     "repo": repo,
                     "read": True,  # All teams returned at least have read
@@ -84,14 +141,16 @@ class OrganizationReport(BaseGithubTask):
                     "admin": repo.permissions["admin"] is True,
                 }
                 teams[team.name]["repos"][repo.name] = info
-        return teams
+        return teams, ignored
 
     def _fetch_repos(self, org, org_members, teams):
         repos = {}
+        ignored = set()
         self.logger.info("Fetching repos...")
         for repo in org.repositories():
             if self.repos and repo.name not in self.repos:
-                self.logger.warning(f"Skipping {repo.name}")
+                self.logger.info(f"Skipping {repo.name}")
+                ignored.add(repo.name)
                 continue
             repos[repo.name] = {
                 "obj": repo,
@@ -166,9 +225,9 @@ class OrganizationReport(BaseGithubTask):
                 if from_team is False:
                     repos[repo.name]["users_direct"][username] = user_info
 
-        return repos
+        return repos, ignored
 
-    def _report(self, org, org_members, teams, repos):
+    def _report(self, org, org_members, teams, repos, extra_fields, ignored):
         self.logger.info("Writing report to {output}".format(**self.options))
         path = os.path.join(cumulusci.__location__, "tasks", "qar", "templates",)
         environment = Environment(loader=FileSystemLoader(path))
@@ -184,6 +243,8 @@ class OrganizationReport(BaseGithubTask):
                     now=datetime.datetime.now(),
                     check=lambda x: "&#x2713;" if x else "",
                     commit=self.project_config.repo_commit,
+                    extra_fields=extra_fields,
+                    ignored=ignored,
                     link_style=False,  # change this for rapid changing of the CSS
                 )
             )
