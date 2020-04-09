c300145abef95a9c5ec40f894cf792b68e360278
Paul Prescod <pprescod@salesforce.com>
2020-04-09 09:49:49 -0700

    Let the data-blizzard begin.

--- cumulusci/tasks/bulkdata/generate_and_load_data.py
+++ cumulusci/tasks/bulkdata/generate_and_load_data.py
@@ -93,7 +93,12 @@ class GenerateAndLoadData(BaseSalesforceApiTask):
             self.mapping_file = None
 
         self.database_url = self.options.get("database_url")
-        self.num_records = int(self.options["num_records"])
+        num_records = self.options.get("num_records")
+        if not num_records:
+            raise TaskOptionsError(
+                "Please specify the number of records to generate with num_records"
+            )
+        self.num_records = int(num_records)
         self.batch_size = int(self.options.get("batch_size", self.num_records))
         if self.batch_size <= 0:
             raise TaskOptionsError("Batch size should be greater than zero")
