0d06618813aad176b33675aaa0a86c9f1c36a41d
Paul Prescod <pprescod@salesforce.com>
2020-04-17 11:17:50 -0700

    WIP

--- schema2factory.py
+++ schema2factory.py
@@ -1,6 +1,10 @@
+import os
+import re
+from glob import glob
 import json
 import sys
 from typing import Tuple
+from pathlib import Path
 
 from faker import Faker
 from faker.providers import BaseProvider
@@ -19,44 +23,82 @@ relevant = [
     "Opportunity",
     "Partial_Soft_Credit__c",
     "npe01__OppPayment__c",
-    # "npe03__Recurring_Donation__c",
+    "npe03__Recurring_Donation__c",
     "npe4__Relationship__c",
 ]
-
-# irrelevant_fields = [
-#     ("*", "LastModifiedDate"), ("*", "IsDeleted"), ("*", "CreatedDate"), ("*", "SystemModstamp"),
-#     ("Partial_Soft_Credit__c", "Contact_Name__c"), ("Partial_Soft_Credit__c", "Name"),
-#     ("*", "LastViewedDate"), ("*", "LastReferencedDate"), ("npe4__Relationship__c", "Name"),
-
-# ]
+IGNORED_FIELDS = [
+    ("*", "jigsaw"),
+    ("*", "CleanStatus"),
+    ("*", "npe01__Contact_Id_for_Role__c"),
+]
 
 
 def with_defaults(field, defaults):
     return {**defaults, **field}
 
 
-def schema2factory(filename, relevant_sobjects):
+def versioned_file(path):
+    path = Path(path)
+    index = 1
+    new_path = path
+    while new_path.exists():
+        new_path = Path(f"{path.stem}_{index}{path.suffix}")
+        index += 1
+    return new_path
+
+
+def schema2factory(filename, relevant_sobjects, out_macro_yml, out_wrapper_yml):
     with open(filename) as file:
         schema = json.load(file)
 
     defaults, objects = schema["defaults"], schema["sobjects"]
-    objs = [
-        render_object(obj, defaults)
+
+    chosen_objects = {
+        objname: obj
         for objname, obj in objects.items()
         if objname in relevant_sobjects and ONLY_RELEVANT
-    ]
-    yaml.dump(objs, sys.stdout, sort_keys=False)
+    }
+    out_macro_versioned = versioned_file(out_macro_yml)
+    render_macro_file(chosen_objects, out_macro_versioned, defaults)
+    out_wrapper_versioned = versioned_file(out_wrapper_yml)
+    render_wrapper_file(chosen_objects, out_wrapper_versioned, out_macro_versioned)
 
 
-def render_object(obj, defaults):
+def render_macro_file(objects, out_macro_versioned, defaults):
+
+    objs = [render_macro(obj, defaults, objects) for objname, obj in objects.items()]
+
+    with open(out_macro_versioned, "w") as f:
+        yaml.dump(objs, f, sort_keys=False)
+
+
+def render_wrapper_file(objects, out_wrapper_versioned, out_macro_yml):
+    objs = [render_reference(objname) for objname in objects]
+
+    with open(out_wrapper_versioned, "w") as f:
+        out = [{"include_file": str(out_macro_yml)}] + objs
+        yaml.dump(out, f, sort_keys=False)
+
+
+def render_reference(objname):
+    return {"object": objname, "include": f"incl_{objname}", "fields": {}}
+
+
+def render_macro(obj, defaults, objects):
     rc = {}
-    rc["object"] = obj["name"]
-    rc["fields"] = dict(
-        render_field(field_name, with_defaults(field, defaults))
+    rc["macro"] = f"incl_{obj['name']}"
+    fields = {
+        field_name: with_defaults(field, defaults)
         for field_name, field in obj["fields"].items()
-        if field.get("createable")
-        # and field_name != "Id" and (obj["name"], field_name) not in irrelevant_fields
-        # and ("*", field_name) not in irrelevant_fields
+    }
+    rc["fields"] = dict(
+        render_field(field_name, field, defaults, objects)
+        for field_name, field in fields.items()
+        if field["createable"]
+        and not field["defaultedOnCreate"]
+        and field_name != "Id"
+        and (obj["name"], field_name) not in IGNORED_FIELDS
+        and ("*", field_name) not in IGNORED_FIELDS
     )
     rc["fields"] = {k: v for k, v in rc["fields"].items() if v is not None}
     return rc
@@ -112,7 +154,10 @@ known_types = {
     "Year Started": KnownType(simple_fake("year"), "string"),
     "code": KnownType(simple_fake("postalcode"), "string"),
     "double": KnownType(
-        lambda **args: {"random_number": {"min": 1, "max": 100000}}, ("float", "double")
+        lambda precision=3, scale=0, **args: {
+            "random_number": {"min": 1, "max": 10 ** (precision - scale)}
+        },
+        ("float", "double"),
     ),
     "int": KnownType(
         lambda **args: {"random_number": {"min": 1, "max": 100000}}, ("int")
@@ -130,16 +175,12 @@ known_types = {
     ),
     "latitude": KnownType(simple_fake("latitude"), ("string", "int", "double")),
     "longitude": KnownType(simple_fake("longitude"), ("string", "int", "double")),
-    "NumberofLocations__c": KnownType(
-        lambda **args: {"random_number": {"min": 1, "max": 20}},
-        ("int", "float", "double"),
-    ),
-    "Geolocation__Latitude__s": KnownType(
-        simple_fake("latitude"), ("string", "int", "double")
-    ),
-    "Geolocation__Longitude__s": KnownType(
-        simple_fake("longitude"), ("string", "int", "double")
-    ),
+    # "Geolocation__Latitude__s": KnownType(
+    #     simple_fake("latitude"), ("string", "int", "double")
+    # ),
+    # "Geolocation__Longitude__s": KnownType(
+    #     simple_fake("longitude"), ("string", "int", "double")
+    # ),
 }
 
 
@@ -157,7 +198,7 @@ def lookup_known_type(name):
     return KnownType(None, "")
 
 
-def render_field(name, field):
+def render_field(name, field, defaults, objects):
     field_type = field["type"]
 
     # picklists
@@ -193,12 +234,20 @@ def render_field(name, field):
 
     # references
     if field["type"] == "reference":
-        return (
-            f"__{name}",
-            f"DISABLED: REFERENCES NOT SUPPORTED YET: {field['referenceTo']}",
-        )
+        if not field["nillable"]:
+            assert isinstance(field["referenceTo"], list)
+            target = field["referenceTo"][0]
+            return (
+                name,
+                {"reference": [render_reference(target)]},
+            )
+        else:
+            return (
+                f"__{name}",
+                f"OPTIONAL REFERENCE SKIPPED: {field['referenceTo']}",
+            )
 
     return (f"__{name}", f"DISABLED: UNKNOWN TYPE: {field['type']}")
 
 
-schema2factory("out.json", relevant)
+schema2factory("out.json", relevant, "generated_macros.yml", "generated_file.yml")
