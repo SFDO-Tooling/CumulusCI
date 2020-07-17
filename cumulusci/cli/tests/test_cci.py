e7a654ed7884d202ce51e3f1d7087c512b15abd0
Paul Prescod <pprescod@salesforce.com>
2020-07-17 13:34:09 -0700

    Revert changes to tests.

--- cumulusci/cli/tests/test_cci.py
+++ cumulusci/cli/tests/test_cci.py
@@ -893,27 +893,19 @@ Environment Info: Rossian / x68_46
             status=200,
         )
         responses.add("GET", "https://instance/services/data", json=[{"version": 45.0}])
-        org_config = False
-
-        def wrapper(*args, **kwargs):
-            nonlocal org_config
-            org_config = OrgConfig(*args, **kwargs)
-            org_config.save = mock.Mock()
-            return org_config
-
-        with mock.patch("cumulusci.cli.cci.OrgConfig", wrapper):
-            run_click_command(
-                cci.org_connect,
-                runtime=runtime,
-                org_name="test",
-                sandbox=False,
-                login_url="https://login.salesforce.com",
-                default=True,
-                global_org=False,
-            )
+        run_click_command(
+            cci.org_connect,
+            runtime=runtime,
+            org_name="test",
+            sandbox=False,
+            login_url="https://login.salesforce.com",
+            default=True,
+            global_org=False,
+        )
 
         runtime.check_org_overwrite.assert_called_once()
-        org_config.save.assert_called_once()
+        runtime.keychain.set_org.assert_called_once()
+        org_config = runtime.keychain.set_org.call_args[0][0]
         assert org_config.expires == "Persistent"
         runtime.keychain.set_default_org.assert_called_once_with("test")
 
