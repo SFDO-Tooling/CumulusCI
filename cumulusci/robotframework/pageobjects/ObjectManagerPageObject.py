1f8631c7c88e3fe62a20de6b3f8dded77ea1ea1f
Ratna <rjanjanam@hotmail.com>
2020-07-28 14:44:58 -0700

    removing the unwanted parameter

--- cumulusci/robotframework/pageobjects/ObjectManagerPageObject.py
+++ cumulusci/robotframework/pageobjects/ObjectManagerPageObject.py
@@ -44,7 +44,7 @@ class ObjectManagerPage(BasePage):
     def _go_to_page(self):
         url_template = "{root}/lightning/setup/ObjectManager/home"
         url = url_template.format(
-            root=self.cumulusci.org.lightning_base_url, object_name=self.object_name
+            root=self.cumulusci.org.lightning_base_url
         )
         self.selenium.go_to(url)
         object_name = self.object_name
