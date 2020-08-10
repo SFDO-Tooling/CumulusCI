23c48c22c33856cdba09dc9801a153b4a536a380
jofsky <josh.kofsky@gmail.com>
2020-08-10 15:20:48 -0700

    Stripping out any changes not specifically made to test_push_tasks.py and tasks.py

--- cumulusci/tasks/push/pushfails.py
+++ cumulusci/tasks/push/pushfails.py
@@ -12,7 +12,7 @@ from cumulusci.tasks.salesforce import BaseSalesforceApiTask
 
 
 class ReportPushFailures(BaseSalesforceApiTask):
-    """ Produce a report of the failed and otherwise anomalous push jobs.
+    """Produce a report of the failed and otherwise anomalous push jobs.
 
 Takes a push request id and writes results to a CSV file. The task result contains the filename.
 """
