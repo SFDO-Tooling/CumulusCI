9d8d5869933fde9d8b3d83e1ae7224d8e50b7baf
jofsky <josh.kofsky@gmail.com>
2020-08-25 17:09:36 -0700

    [CCI] Remove orgs option requirement adding csv and csv_field_names

--- cumulusci/tasks/push/tests/test_push_tasks.py
+++ cumulusci/tasks/push/tests/test_push_tasks.py
@@ -1,4 +1,3 @@
-import os
 import datetime
 import pytest
 
@@ -16,31 +15,15 @@ ORG_FILE_TEXT = """\n00DS0000003TJJ6MAO\n00DS0000003TJJ6MAL"""
 REAL_ORG_FILE_TEXT = """OrganizationId,OrgName,OrgType,OrgStatus,InstanceName,ErrorSeverity,ErrorTitle,ErrorType,ErrorMessage,Gack Id,Stacktrace Id,\n00D5w000004zXXX,,,,,Error,Unexpected Failure,UnclassifiedError,An unexpected failure was experienced during the upgrade. The subscriber's organization was unaffected. Contact salesforce.com Support through your normal channels and provide the following error number: 1351793968-113330 (-1345328791).,1351793968-113330,-1345328791\n00D5w000005VXXX,,,,,Error,Unexpected Failure,UnclassifiedError,An unexpected failure was experienced during the upgrade. The subscriber's organization was unaffected. Contact salesforce.com Support through your normal channels and provide the following error number: 822524189-80345 (-2096886284).,822524189-80345,-2096886284"""
 
 
-@pytest.fixture
-def org_file():
-    with open(ORG_FILE, "w") as file:
-        file.write(ORG_FILE_TEXT)
-    try:
-        yield  # this is where the test using the fixture runs
-    finally:
-        os.remove(ORG_FILE)
-
-
-@pytest.fixture
-def real_org_file():
-    with open(ORG_FILE, "w") as file:
-        file.write(REAL_ORG_FILE_TEXT)
-    try:
-        yield  # this is where the test using the fixture runs
-    finally:
-        os.remove(ORG_FILE)
-
-
-def test_schedule_push_org_list_get_orgs_formatted(org_file):
+def test_schedule_push_org_list_get_orgs_formatted(tmp_path):
+    tmp = tmp_path / "tmp"
+    tmp.mkdir()
+    orgs = tmp / ORG_FILE
+    orgs.write_text(ORG_FILE_TEXT)
     task = create_task(
         SchedulePushOrgList,
         options={
-            "orgs": ORG_FILE,
+            "orgs": orgs,
             "version": VERSION,
             "namespace": NAMESPACE,
             "start_time": datetime.datetime.now(),
@@ -50,11 +33,15 @@ def test_schedule_push_org_list_get_orgs_formatted(org_file):
     assert task._get_orgs() == ["00DS0000003TJJ6MAO", "00DS0000003TJJ6MAL"]
 
 
-def test_schedule_push_org_list_get_orgs(real_org_file):
+def test_schedule_push_org_list_get_orgs(tmp_path):
+    tmp = tmp_path / "tmp"
+    tmp.mkdir()
+    orgs = tmp / ORG_FILE
+    orgs.write_text(REAL_ORG_FILE_TEXT)
     task = create_task(
         SchedulePushOrgList,
         options={
-            "orgs": ORG_FILE,
+            "orgs": orgs,
             "version": VERSION,
             "namespace": NAMESPACE,
             "start_time": datetime.datetime.now(),
@@ -66,12 +53,16 @@ def test_schedule_push_org_list_get_orgs(real_org_file):
     assert task._get_orgs() == ["00D5w000004zXXX", "00D5w000005VXXX"]
 
 
-def test_schedule_push_org_list_get_error(real_org_file):
+def test_schedule_push_org_list_get_error(tmp_path):
+    tmp = tmp_path / "tmp"
+    tmp.mkdir()
+    orgs = tmp / ORG_FILE
+    orgs.write_text(REAL_ORG_FILE_TEXT)
     with pytest.raises(TaskOptionsError):
         task = create_task(
             SchedulePushOrgList,
             options={
-                "orgs": ORG_FILE,
+                "orgs": orgs,
                 "version": VERSION,
                 "namespace": NAMESPACE,
                 "start_time": datetime.datetime.now(),
@@ -83,11 +74,15 @@ def test_schedule_push_org_list_get_error(real_org_file):
 
 
 # Should set csv_field_name to OrganizationId by default
-def test_schedule_push_org_list_get_default(org_file):
+def test_schedule_push_org_list_get_default(tmp_path):
+    tmp = tmp_path / "tmp"
+    tmp.mkdir()
+    orgs = tmp / ORG_FILE
+    orgs.write_text(ORG_FILE_TEXT)
     task = create_task(
         SchedulePushOrgList,
         options={
-            "orgs": ORG_FILE,
+            "orgs": orgs,
             "version": VERSION,
             "namespace": NAMESPACE,
             "start_time": datetime.datetime.now(),
@@ -98,7 +93,11 @@ def test_schedule_push_org_list_get_default(org_file):
     assert task.options["csv_field_name"] == "OrganizationId"
 
 
-def test_schedule_push_org_list_no_file(org_file):
+def test_schedule_push_org_list_no_file(tmp_path):
+    tmp = tmp_path / "tmp"
+    tmp.mkdir()
+    orgs = tmp / ORG_FILE
+    orgs.write_text(ORG_FILE_TEXT)
     with pytest.raises(TaskOptionsError):
         task = create_task(
             SchedulePushOrgList,
