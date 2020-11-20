82cd7343834fe376b2d318195360026fc19c355b
Beth Breisnes <bbreisnes@salesforce.com>
2020-11-19 17:03:04 -0800

    wip

--- cumulusci/tasks/metadata_etl/encryption.py
+++ cumulusci/tasks/metadata_etl/encryption.py
@@ -1,16 +1,17 @@
 import os
 import yaml
 
+from cumulusci.tasks.salesforce.BaseSalesforceApiTask import BaseSalesforceApiTask
 from cumulusci.tasks.metadata_etl.base import BaseMetadataSynthesisTask
 from cumulusci.utils import os_friendly_path
 from cumulusci.utils.xml import metadata_tree
 # from cumulusci.utils import process_list_arg
 
 
-class EncryptAllFields(BaseMetadataSynthesisTask):
+class EncryptAllFields(BaseSalesforceApiTask, BaseMetadataSynthesisTask):
 
     task_options = {
-        "settings_path": {
+        "blocklist_path": {
             "description": "The path to a YAML settings file",
             "required": False,
         }
@@ -44,8 +45,8 @@ class EncryptAllFields(BaseMetadataSynthesisTask):
     def _create_sobject_element(self):
         return metadata_tree.fromstring(
             b"""<?xml version="1.0" encoding="UTF-8"?>
-                ... <CustomObject xmlns="http://soap.sforce.com/2006/04/metadata">
-                ... </CustomObject>"""
+<CustomObject xmlns="http://soap.sforce.com/2006/04/metadata">
+</CustomObject>"""
         )
 
     def _synthesize(self):
@@ -54,25 +55,29 @@ class EncryptAllFields(BaseMetadataSynthesisTask):
             self.blocklist = yaml.safe_load(f)
 
         print(self.blocklist)
-        # for sobject in self.sf.describe()["sobjects"]:
-        #     sobject_name = sobject["name"]
+        print(self.sf.describe()["sobjects"][1].keys())
+        for sobject in self.sf.describe()["sobjects"]:
+            print(sobject.keys)
+            sobject_name = sobject["name"]
+            print(sobject_name)
 
-        #     root_element = self._create_sobject_element()
+            root_element = self._create_sobject_element()
+            for field in sobject["fields"]:
+                field_name = field["name"]
+                print(field_name)
+                if (
+                    self._is_field_encryptable(field)
+                    and f"{sobject_name}.{field_name}" not in self.blocklist
+                ):
+                    print(f"{field_name}")
+                    field_element = root_element.append("fields")
+                    field_element.append("fullName", text=field_name)
+                    field_element.append("encryptionScheme", "ProbabilisticEncryption")
 
-        #     for field in sobject["fields"]:
-        #         field_name = field["name"]
-        #         if (
-        #             self._is_field_encryptable(field)
-        #             and f"{sobject_name}.{field_name}" not in self.blocklist
-        #         ):
-        #             field_element = root_element.append("fields")
-        #             field_element.append("fullName", text=field_name)
-        #             field_element.append("encryptionScheme", "ProbabilisticEncryption")
+            object_dir = self.deploy_dir / "objects"
+            if not object_dir.exists():
+                object_dir.mkdir()
 
-        #     object_dir = self.deploy_dir / "objects"
-        #     if not object_dir.exists():
-        #         object_dir.mkdir()
-
-        #     (object_dir / f"{sobject_name}.object").write_text(
-        #         root_element.tostring(xml_declaration=True), encoding="utf-8"
-        #     )
+            (object_dir / f"{sobject_name}.object").write_text(
+                root_element.tostring(xml_declaration=True), encoding="utf-8"
+            )
