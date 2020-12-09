83bd7b5cc0c8145aad975361f728f813d614c8a2
Paul Prescod <pprescod@salesforce.com>
2020-12-09 14:42:41 -0800

    PR responses

--- cumulusci/utils/xml/test/test_metadata_tree.py
+++ cumulusci/utils/xml/test/test_metadata_tree.py
@@ -212,29 +212,20 @@ class TestMetadataTree:
         CustomMetadata = fromstring(xml)
         assert xml.strip() == CustomMetadata.tostring().strip()
 
-    def test_namespaced_to_string(self):
-        xml = """
-<CustomMetadata xmlns="http://soap.sforce.com/2006/04/metadata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <label>Account MD Isolation Rollup</label>
-    <protected>false</protected>
-    <values>
-        <field>dlrs__Active__c</field>
-        <value xsi:type="xsd:boolean">true</value>
-    </values>
-</CustomMetadata>""".strip()
+    def test_namespaced_to_string__do_not_output_namespaces(self):
+        CustomMetadata = fromstring(standard_xml)
 
-        CustomMetadata = fromstring(xml)
+        xml_out = CustomMetadata.find("bar").find("name").tostring()
+        assert xml_out.strip() == "<name>Bar1</name>", xml_out.strip()
 
-        xml_out = CustomMetadata.find("values").find("field").tostring()
-        assert xml_out.strip() == "<field>dlrs__Active__c</field>", xml_out.strip()
-
+    def test_namespaced_to_string__output_namespaces(self):
+        CustomMetadata = fromstring(standard_xml)
         xml_out = (
-            CustomMetadata.find("values")
-            .find("field")
+            CustomMetadata.find("bar")
+            .find("name")
             .tostring(xml_declaration=True, include_parent_namespaces=True)
         )
-        expected_out = """<?xml version="1.0" encoding="UTF-8"?>
-             <field xmlns="http://soap.sforce.com/2006/04/metadata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">dlrs__Active__c</field>"""
+        expected_out = """<?xml version="1.0" encoding="UTF-8"?> <name xmlns="http://soap.sforce.com/2006/04/metadata">Bar1</name>"""
         assert (
             " ".join(xml_out.split()).strip() == " ".join(expected_out.split()).strip()
         ), xml_out.strip()
