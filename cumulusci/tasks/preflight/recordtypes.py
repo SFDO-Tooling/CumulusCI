c63400e64b1810e6b8a9892d47d1df26da69b9a9
Clay Tomerlin <ctomerlin@salesforce.com>
2021-02-05 12:42:44 -0800

    Recordtype check task

---
+++ cumulusci/tasks/preflight/recordtypes.py
@@ -0,0 +1,21 @@
+from cumulusci.tasks.salesforce import BaseSalesforceApiTask
+
+class GetRecordTypesForSobject(BaseSalesforceApiTask):
+    task_options = {
+        "sobject": {
+            "description": "The sObject for which we want check if any recordtypes exist",
+            "required": True,
+        }
+    }
+
+    def _run_task(self):
+        sobject = self.options["sobject"]
+        results = self.tooling.query(
+            f"Select  Name FROM RecordType WHERE SobjectType = '{sobject}'"
+        )["records"]
+        self.return_values = bool(results)
+        self.logger.info(
+            f"Found existing recordtype for {sobject}: {self.return_values}"
+        )
+        if self.return_values:
+            self.logger.info(f"Found record types for Sobjects:\n{results}")
