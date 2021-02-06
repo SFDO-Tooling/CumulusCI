4d3a092ecde2cf4f7d1644ae2e3d6b2c7cc125a9
Clay Tomerlin <ctomerlin@salesforce.com>
2021-02-05 17:11:23 -0800

    updated to strip fullname of sobject prefix, and fixed test

--- cumulusci/tasks/preflight/recordtypes.py
+++ cumulusci/tasks/preflight/recordtypes.py
@@ -1,15 +1,15 @@
 from cumulusci.tasks.salesforce import BaseSalesforceApiTask
 from collections import defaultdict
+
+
 class CheckSObjectRecordTypes(BaseSalesforceApiTask):
     def _run_task(self):
         rts = defaultdict(list)
-        records = self.tooling.query(
-            f"Select SObjectType,FullName FROM RecordType"
-        )["records"]
+        records = self.tooling.query("Select SObjectType,FullName FROM RecordType")[
+            "records"
+        ]
         for r in records:
-            rts[r['SobjectType']].append(r['FullName'])
-        
-        self.return_values = rts                
-        self.logger.info(
-            f"Found existing recordtypes: {self.return_values}"
-        )        
+            rts[r["SobjectType"]].append(r["FullName"].split(".")[1])
+
+        self.return_values = rts
+        self.logger.info(f"Found existing recordtypes: {self.return_values}")
--- cumulusci/tasks/preflight/tests/test_recordtypes.py
+++ cumulusci/tasks/preflight/tests/test_recordtypes.py
@@ -1,27 +1,31 @@
 from unittest.mock import Mock
 import unittest
 
-from cumulusci.tasks.preflight.recordtypes import (
-    CheckSObjectRecordTypes    
-)
+from cumulusci.tasks.preflight.recordtypes import CheckSObjectRecordTypes
 from cumulusci.tasks.salesforce.tests.util import create_task
 
 
 class TestRecordTypePreflights(unittest.TestCase):
     def test_recordType_preflight(self):
         task = create_task(CheckSObjectRecordTypes, {})
-        task._init_api = Mock()
-        task._init_api.return_value.query.return_value = {
+        task.tooling = Mock()
+        task.tooling.query.return_value = {
             "totalSize": 3,
             "records": [
-                {"SobjectType": "Account", "FullName" : "Account.Business_Account"},
-                {"SobjectType": "Account", "FullName" : "PersonAccount.PersonAccount"},
-                {"SobjectType": "ActionPlanTemplate", "FullName" : "ActionPlanTemplate.Default"}
+                {"SobjectType": "Account", "FullName": "Account.Business_Account"},
+                {"SobjectType": "Account", "FullName": "PersonAccount.PersonAccount"},
+                {
+                    "SobjectType": "ActionPlanTemplate",
+                    "FullName": "ActionPlanTemplate.Default",
+                },
             ],
         }
-        task()
+        task._run_task()
 
-        task._init_api.return_value.query.assert_called_once_with(
+        task.tooling.query.assert_called_once_with(
             "Select SObjectType,FullName FROM RecordType"
         )
-        assert task.return_values == {'Account': ['Account.Business_Account', 'PersonAccount.PersonAccount'], 'ActionPlanTemplate': ['ActionPlanTemplate.Default']}
+        assert task.return_values == {
+            "Account": ["Business_Account", "PersonAccount"],
+            "ActionPlanTemplate": ["Default"],
+        }
