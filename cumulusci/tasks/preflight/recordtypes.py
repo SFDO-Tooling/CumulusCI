90bdd8932208dfcd44b926601c57a131a724abe4
David Reed <d.reed@salesforce.com>
2021-02-09 09:47:33 -0700

    query_all instead of query

--- cumulusci/tasks/preflight/recordtypes.py
+++ cumulusci/tasks/preflight/recordtypes.py
@@ -5,9 +5,9 @@ from collections import defaultdict
 class CheckSObjectRecordTypes(BaseSalesforceApiTask):
     def _run_task(self):
         rts = defaultdict(list)
-        records = self.tooling.query("Select SobjectType, FullName FROM RecordType")[
-            "records"
-        ]
+        records = self.tooling.query_all(
+            "Select SobjectType, FullName FROM RecordType"
+        )["records"]
         for r in records:
             rts[r["SobjectType"]].append(r["FullName"].split(".")[1])
 
