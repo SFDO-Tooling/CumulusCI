a6e1a5fb1be934f86c72536133d109dd4bcf0f0f
Paul Prescod <pprescod@salesforce.com>
2021-02-23 17:02:53 -0800

    Docstrings and datatypes

--- cumulusci/utils/iterators.py
+++ cumulusci/utils/iterators.py
@@ -1,8 +1,9 @@
 import itertools
-import typing
+import typing as T
 
 
-def iterate_in_chunks(n: int, iterable: typing.Iterable):
+def iterate_in_chunks(n: int, iterable: T.Iterable) -> T.Iterable[T.Tuple]:
+    """Split an iterable into chunks of size 'n'."""
     it = iter(iterable)
     while True:
         chunk = tuple(itertools.islice(it, n))
