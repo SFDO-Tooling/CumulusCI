1809501ef43c36474d2952704a838e1abca5e4d7
Brandon Parker <brandon.parker@salesforce.com>
2021-03-12 13:49:05 -0800

    remove duplicate fixtures

--- cumulusci/core/tests/test_config_util.py
+++ cumulusci/core/tests/test_config_util.py
@@ -6,7 +6,7 @@ import yaml
 from unittest import mock
 
 from cumulusci.utils import temporary_dir, touch
-from cumulusci.core.config import OrgConfig, ServiceConfig
+from cumulusci.core.config import ServiceConfig
 from cumulusci.core.config import UniversalConfig
 from cumulusci.core.config import SfdxOrgConfig
 from cumulusci.core.keychain import BaseProjectKeychain
@@ -73,30 +73,6 @@ def project_config(repo_root):
     project_config.get_github_api = mock.Mock()
 
     return project_config
-
-
-@pytest.fixture
-def devhub_config():
-    org_config = OrgConfig(
-        {"instance_url": "https://devhub.my.salesforce.com", "access_token": "token"},
-        "devhub",
-    )
-    org_config.refresh_oauth_token = mock.Mock()
-    return org_config
-
-
-@pytest.fixture
-def org_config():
-    org_config = OrgConfig(
-        {
-            "instance_url": "https://scratch.my.salesforce.com",
-            "access_token": "token",
-            "config_file": "orgs/scratch_def.json",
-        },
-        "dev",
-    )
-    org_config.refresh_oauth_token = mock.Mock()
-    return org_config
 
 
 def test_get_devhub_config__from_sfdx(project_config):
