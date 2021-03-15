e169e9340581003d5a36e8b303f9f77478831c3f
Velvel Marasow <vmarasow@salesforce.com>
2021-03-15 12:02:20 -0700

    Allow a user to retrieve the list of available permsets

--- cumulusci/tasks/preflight/tests/test_licenses.py
+++ cumulusci/tasks/preflight/tests/test_licenses.py
@@ -4,6 +4,7 @@ import unittest
 from cumulusci.tasks.preflight.licenses import (
     GetAvailableLicenses,
     GetAvailablePermissionSetLicenses,
+    GetAvailablePermissionSets
 )
 from cumulusci.tasks.salesforce.tests.util import create_task
 
@@ -41,5 +42,23 @@ class TestLicensePreflights(unittest.TestCase):
 
         task._init_api.return_value.query.assert_called_once_with(
             "SELECT PermissionSetLicenseKey FROM PermissionSetLicense"
+        )
+        assert task.return_values == ["TEST1", "TEST2"]
+
+    def test_permsets_preflight(self):
+        task = create_task(GetAvailablePermissionSets, {})
+        task._init_api = Mock()
+
+        task._init_api.return_value.query.return_value = {
+            "totalSize": 2,
+            "records": [
+                {"Name": "TEST1"},
+                {"Name": "TEST2"},
+            ],
+        }
+        task()
+
+        task._init_api.return_value.query.assert_called_once_with(
+            "SELECT Name FROM PermissionSet"
         )
         assert task.return_values == ["TEST1", "TEST2"]
