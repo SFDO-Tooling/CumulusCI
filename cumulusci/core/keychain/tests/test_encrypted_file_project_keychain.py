0c5efd58648ecd65dfc31b0e0a00c1255f0b745d
Brandon Parker <brandon.parker@salesforce.com>
2021-04-06 12:46:59 -0700

    fix typo

--- cumulusci/core/keychain/tests/test_encrypted_file_project_keychain.py
+++ cumulusci/core/keychain/tests/test_encrypted_file_project_keychain.py
@@ -38,7 +38,7 @@ class TestEncryptedFileProjectKeychain:
     def test_cache_dir(self, keychain):
         assert keychain.cache_dir.name == ".cci"
 
-    def test_raise_service_not_configred(self, keychain):
+    def test_raise_service_not_configured(self, keychain):
         with pytest.raises(ServiceNotConfigured):
             keychain._raise_service_not_configured("test-name")
 
