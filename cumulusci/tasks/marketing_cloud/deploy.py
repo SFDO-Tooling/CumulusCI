a2b6c22d85180034abd095a82bd6a3e66f1d5128
David Glick <dglick@salesforce.com>
2021-05-13 21:32:55 -0400

    MC package deploy fixes

--- cumulusci/tasks/marketing_cloud/deploy.py
+++ cumulusci/tasks/marketing_cloud/deploy.py
@@ -36,11 +36,11 @@ class MarketingCloudDeployTask(BaseMarketingCloudTask):
             self.logger.error(f"Package zip file not valid: {pkg_zip_file.name}")
             return
 
-        with temporary_dir() as temp_dir:
+        with temporary_dir(chdir=False) as temp_dir:
             with zipfile.ZipFile(pkg_zip_file) as zf:
                 zf.extractall(temp_dir)
                 payload = self._construct_payload(
-                    Path(f"{temp_dir}/{pkg_zip_file.stem}")
+                    Path(f"{temp_dir}/{zf.namelist()[0]}")
                 )
 
         response = requests.post(
