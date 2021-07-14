43de0c5374b0f0a616ad386293d04639922f9398
Brandon Parker <brandon.parker@salesforce.com>
2021-07-14 14:44:50 -0700

    add note about EnvironmentProjectKeychain

--- cumulusci/core/keychain/environment_project_keychain.py
+++ cumulusci/core/keychain/environment_project_keychain.py
@@ -2,4 +2,6 @@ from cumulusci.core.keychain.encrypted_file_project_keychain import (
     EncryptedFileProjectKeychain,
 )
 
+# All logic for loading orgs and services from the environment
+# now lives in EncryptedFileProjectKeychain.
 EnvironmentProjectKeychain = EncryptedFileProjectKeychain  # pragma: no cover
