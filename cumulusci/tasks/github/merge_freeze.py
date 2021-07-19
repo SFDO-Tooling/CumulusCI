aa060cd478015097d7a94607c0ae3b83bbfe947d
Josh Kofsky <jkofsky@salesforce.com>
2021-07-19 16:03:37 -0400

    Update cumulusci/tasks/github/merge_freeze.py
    Co-authored-by: Brandon Parker <brandon.parker@salesforce.com>

--- cumulusci/tasks/github/merge_freeze.py
+++ cumulusci/tasks/github/merge_freeze.py
@@ -17,4 +17,9 @@ class EnableMergeFreeze(BaseGithubTask):
         for project in self.options["repos"]:
             if project["owner"] and project["repo"]:
                 self.logger.info(f'Enabling merge freeze on {project["owner"]}/{project["repo"]}')
-                self.github.repository(project["owner"], project["repo"]).branch("main").protection().update(restrictions={"users": [], "teams": []})
+                repo = self.github.repository(project["owner"], project["repo"])
+                self._restrict_access_on_repo(repo)
+                        
+   def _restrict_access_on_repo(repo):
+       '''Restricts access on a repo so that....'''
+       repo.branch("main").protection().update(restrictions={"users": [], "teams": []})
