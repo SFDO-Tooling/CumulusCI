5af0a007ac2793bbdb426342e103ff68d0ac0ca0
David Ray <dray@salesforce.com>
2021-08-23 13:51:19 -0700

    Fix typo in task docs

--- cumulusci/tasks/metadata_etl/layouts.py
+++ cumulusci/tasks/metadata_etl/layouts.py
@@ -96,7 +96,7 @@ class AddFieldsToPageLayout(MetadataSingleEntityTransformTask):
 
         - pages:
 
-            - visualforce_page_name:[page_name]
+            - api_name:[page_name]
             - height: int (Optional. Default: 200)
             - show_label: Boolean (Optional. Default: False)
             - show_scrollbars: Boolean (Optional. Default: False)
