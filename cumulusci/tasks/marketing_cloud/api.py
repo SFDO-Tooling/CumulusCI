78020099617c1441ad5e663297dbe4c2c559f6c0
Marcel Pufal <marcel.pufal@salesforce.com>
2021-08-30 23:20:36 -0700

    roll in the other tasks

--- cumulusci/tasks/marketing_cloud/api.py
+++ cumulusci/tasks/marketing_cloud/api.py
@@ -49,3 +49,157 @@ class MarketingCloudCreateSubscriberAttribute(BaseMarketingCloudTask):
                 f"Error from Marketing Cloud: {status_message} \n\nFull response text: {response.text}"
             )
         self.return_values = {"success": success}
+
+
+class MarketingCloudCreateUser(BaseMarketingCloudTask):
+    task_options = {
+        "parent_bu_mid": {
+            "description": "Specify the MID for Parent BU.",
+            "required": True,
+        },
+        "default_bu_mid": {
+            "description": "Set MID for BU to use as default (can be same as the parent).",
+            "required": True,
+        },
+        "external_key": {
+            "description": "Set the User's external key.",
+            "required": False,
+        },
+        "user_name": {
+            "description": "Set the User's name. Not the same as their username.",
+            "required": False,
+        },
+        "user_email": {
+            "description": "Set the User's email.",
+            "required": True,
+        },
+        "user_password": {
+            "description": "Set the User's password.",
+            "required": True,
+        },
+        "user_username": {
+            "description": "Set the User's username. Not the same as their name.",
+            "required": True,
+        },
+        "role_id": {
+            "description": "Assign a Role to the new User, specified as an ID. IDs for system defined roles located here: https://developer.salesforce.com/docs/atlas.en-us.noversion.mc-apis.meta/mc-apis/setting_user_permissions_via_the_web_services_api.htm",
+            "required": False,
+        },
+    }
+
+    def _run_task(self):
+        # get soap envelope
+        envelope = envelopes.CREATE_USER
+        # fill the merge fields
+        # TO DO: this is ugly: there's probably already a constructor for this
+        # also handle empty or optional inputs as an empty strings into the merge field
+        envelope = envelope.format(
+            soap_instance_url=self.mc_config.soap_instance_url,
+            access_token=self.mc_config.access_token,
+            parent_bu_mid=self.options.get("parent_bu_mid"),
+            default_bu_mid=self.options.get("default_bu_mid"),
+            external_key=self.options.get("external_key"),
+            user_name=self.options.get("user_name"),
+            user_email=self.options.get("user_email"),
+            user_password=self.options.get("user_password"),
+            user_username=self.options.get("user_username"),
+            role_id=self.options.get("role_id"),
+        )
+        # construct request
+        # TO DO: DRY refactor
+        response = requests.post(
+            f"{self.mc_config.soap_instance_url}Service.asmx",
+            data=envelope.encode("utf-8"),
+            headers={"Content-Type": "text/xml; charset=utf-8"},
+        )
+        response.raise_for_status()
+        # check resulting status code
+        # TO DO: DRY refactor
+        root = etree.fromstring(response.content)
+        status_code = root.find(
+            ".//{http://exacttarget.com/wsdl/partnerAPI}StatusCode"
+        ).text
+        status_message = root.find(
+            ".//{http://exacttarget.com/wsdl/partnerAPI}StatusMessage"
+        ).text
+        success = True
+        if status_code == "OK":
+            user_username = self.options.get("user_username")
+            self.logger.info(f"Successfully created User: {user_username}.")
+        if status_code != "OK":
+            raise Exception(
+                f"Error from Marketing Cloud: {status_message}\n\nFull response text: {response.text}"
+            )
+        self.return_values = {"success": success}
+
+
+class MarketingCloudUpdateUserRole(BaseMarketingCloudTask):
+    task_options = {
+        "account_mid": {
+            "description": "Specify the Account MID.",
+            "required": True,
+        },
+        "external_key": {
+            "description": "Specify the User's external key.",
+            "required": False,
+        },
+        "user_name": {
+            "description": "Specify the User's name. Not the same as their username.",
+            "required": False,
+        },
+        "user_email": {
+            "description": "Specify the User's email.",
+            "required": True,
+        },
+        "user_password": {
+            "description": "Specify the User's password.",
+            "required": True,
+        },
+        "role_id": {
+            "description": "Assign a Role to the User, specified as an ID. IDs for system defined roles located here: https://developer.salesforce.com/docs/atlas.en-us.noversion.mc-apis.meta/mc-apis/setting_user_permissions_via_the_web_services_api.htm",
+            "required": True,
+        },
+    }
+
+    def _run_task(self):
+        # get soap envelope
+        envelope = envelopes.UPDATE_USER_ROLE
+        # fill the merge fields
+        # TO DO: this is ugly: there's probably already a constructor for this
+        # also handle empty or optional inputs as an empty strings into the merge field
+        envelope = envelope.format(
+            soap_instance_url=self.mc_config.soap_instance_url,
+            access_token=self.mc_config.access_token,
+            account_mid=self.options.get("account_mid"),
+            external_key=self.options.get("external_key"),
+            user_name=self.options.get("user_name"),
+            user_email=self.options.get("user_email"),
+            user_password=self.options.get("user_password"),
+            role_id=self.options.get("role_id"),
+        )
+        # construct request
+        # TO DO: DRY refactor
+        response = requests.post(
+            f"{self.mc_config.soap_instance_url}Service.asmx",
+            data=envelope.encode("utf-8"),
+            headers={"Content-Type": "text/xml; charset=utf-8"},
+        )
+        response.raise_for_status()
+        # check resulting status code
+        # TO DO: DRY refactor
+        root = etree.fromstring(response.content)
+        status_code = root.find(
+            ".//{http://exacttarget.com/wsdl/partnerAPI}StatusCode"
+        ).text
+        status_message = root.find(
+            ".//{http://exacttarget.com/wsdl/partnerAPI}StatusMessage"
+        ).text
+        success = True
+        if status_code == "OK":
+            user_name = self.options.get("user_name")
+            self.logger.info(f"Successfully updated role for User: {user_name}.")
+        if status_code != "OK":
+            raise Exception(
+                f"Error from Marketing Cloud: {status_message}\n\nFull response text: {response.text}"
+            )
+        self.return_values = {"success": success}
--- cumulusci/tasks/marketing_cloud/tests/test_api.py
+++ cumulusci/tasks/marketing_cloud/tests/test_api.py
@@ -4,51 +4,10 @@ from cumulusci.core.config import ServiceConfig
 from cumulusci.core.config.marketing_cloud_service_config import (
     MarketingCloudServiceConfig,
 )
+from cumulusci.tasks.marketing_cloud.tests import test_api_soap_envelopes as envelopes
 from cumulusci.tests.util import create_project_config
 
 from ..api import MarketingCloudCreateSubscriberAttribute
-
-EXPECTED_SOAP_RESPONSE = """<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing"
-    xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
-    xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-    <env:Header xmlns:env="http://www.w3.org/2003/05/soap-envelope">
-        <wsa:Action>ConfigureResponse</wsa:Action>
-        <wsa:MessageID>urn:uuid:9be492dc-a0b9-4cf9-908b-e02a11d623c3</wsa:MessageID>
-        <wsa:RelatesTo>urn:uuid:b7df082c-4ddb-4a62-8cda-de290f42b9c0</wsa:RelatesTo>
-        <wsa:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To>
-        <wsse:Security>
-            <wsu:Timestamp wsu:Id="Timestamp-61ba3507-784d-4084-bdf0-0ca052fc29ec">
-                <wsu:Created>2021-08-30T19:24:48Z</wsu:Created>
-                <wsu:Expires>2021-08-30T19:29:48Z</wsu:Expires>
-            </wsu:Timestamp>
-        </wsse:Security>
-    </env:Header>
-    <soap:Body>
-        <ConfigureResponseMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">
-            <Results>
-                <Result>
-                    <StatusCode>OK</StatusCode>
-                    <StatusMessage>Success</StatusMessage>
-                    <OrdinalID>0</OrdinalID>
-                    <Object xsi:type="PropertyDefinition">
-                        <PartnerKey xsi:nil="true" />
-                        <ID>126713</ID>
-                        <ObjectID xsi:nil="true" />
-                        <Name>test</Name>
-                        <PropertyType>string</PropertyType>
-                    </Object>
-                </Result>
-            </Results>
-            <OverallStatus>OK</OverallStatus>
-            <OverallStatusMessage />
-            <RequestID>ef959dde-a21d-46ac-b2de-98d9e656ed6f</RequestID>
-        </ConfigureResponseMsg>
-    </soap:Body>
-</soap:Envelope>"""
 
 
 @responses.activate
@@ -93,7 +52,7 @@ def test_marketing_cloud_create_subscriber_attribute_task(create_task):
     responses.add(
         "POST",
         "https://tssd.soap.marketingcloudapis.com/Service.asmx",
-        body=EXPECTED_SOAP_RESPONSE,
+        body=envelopes.CREATE_SUBSCRIBER_ATTRIBUTE_EXPECTED_SOAP_RESPONSE,
     )
 
     task = create_task(
