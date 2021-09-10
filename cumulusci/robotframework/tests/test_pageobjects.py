e1f567014aa3caeaa687c1978c0fecda8731da13
Bryan Oakley <bryan.oakley@gmail.com>
2021-09-10 15:22:51 -0500

    Fixed a lot of issues with this test to allow it to run in random order.

--- cumulusci/robotframework/tests/test_pageobjects.py
+++ cumulusci/robotframework/tests/test_pageobjects.py
@@ -16,6 +16,7 @@ BarTestPage has two.
 import os.path
 import sys
 import unittest
+from contextlib import contextmanager
 from unittest import mock
 
 import pytest
@@ -24,39 +25,12 @@ from robot.libraries.BuiltIn import BuiltIn
 
 from cumulusci.robotframework import PageObjects
 from cumulusci.robotframework.CumulusCI import CumulusCI
-from cumulusci.robotframework.pageobjects import (
-    BasePage,
-    DetailPage,
-    EditModal,
-    HomePage,
-    ListingPage,
-    NewModal,
-    ObjectManagerPage,
-)
 from cumulusci.robotframework.pageobjects.PageObjectLibrary import _PageObjectLibrary
 from cumulusci.utils import temporary_dir
 
 HERE = os.path.dirname(__file__)
 FOO_PATH = os.path.join(HERE, "FooTestPage.py")
 BAR_PATH = os.path.join(HERE, "BarTestPage.py")
-CORE_KEYWORDS = [
-    "current_page_should_be",
-    "get_page_object",
-    "go_to_page",
-    "load_page_object",
-    "log_page_object_keywords",
-    "wait_for_modal",
-    "wait_for_page_object",
-]
-
-BASE_REGISTRY = {
-    ("Detail", ""): DetailPage,
-    ("Edit", ""): EditModal,
-    ("Home", ""): HomePage,
-    ("Listing", ""): ListingPage,
-    ("New", ""): NewModal,
-    ("ObjectManager", ""): ObjectManagerPage,
-}
 
 # this is the importer used by the page objects, which makes it easy
 # peasy to import by file path
@@ -64,6 +38,11 @@ importer = robot.utils.Importer()
 
 
 class MockGetLibraryInstance:
+    """Mock robot's get_library_instance method
+
+    This will return instances of the libraries used in this set of tests.
+    """
+
     libs = {
         "SeleniumLibrary": mock.Mock(),
         "cumulusci.robotframework.CumulusCI": CumulusCI(),
@@ -77,6 +56,20 @@ class MockGetLibraryInstance:
             raise Exception("unknown library: {}".format(libname))
 
 
+@contextmanager
+def reload_PageObjects(*args):
+    # Reset the registry and re-import the built-in page objects
+    saved_registry = PageObjects.registry
+    PageObjects._reset()
+    import cumulusci.robotframework.pageobjects.BasePageObjects  # noqa: F401
+    import cumulusci.robotframework.pageobjects.ObjectManagerPageObject  # noqa: F401
+
+    yield PageObjects(*args)
+
+    PageObjects._reset()
+    PageObjects.registry = saved_registry
+
+
 @mock.patch(
     "robot.libraries.BuiltIn.BuiltIn.get_library_instance",
     side_effect=MockGetLibraryInstance(),
@@ -87,93 +80,120 @@ class MockGetLibraryInstance:
 class TestPageObjects(unittest.TestCase):
     def test_PageObject(self, get_context_mock, get_library_instance_mock):
         """Smoke test to make sure the default registry is set up and keywords exist"""
-        po = PageObjects()
-        assert po.get_keyword_names() == CORE_KEYWORDS, (
-            po.get_keyword_names(),
-            CORE_KEYWORDS,
-        )
-        assert len(po.registry) == len(BASE_REGISTRY), (
-            len(po.registry),
-            len(BASE_REGISTRY),
-        )
-        assert po.registry == BASE_REGISTRY, (po.registry, BASE_REGISTRY)
+
+        with reload_PageObjects() as po:
+            expected_keywords = [
+                "current_page_should_be",
+                "get_page_object",
+                "go_to_page",
+                "load_page_object",
+                "log_page_object_keywords",
+                "wait_for_modal",
+                "wait_for_page_object",
+            ]
+            actual_keywords = po.get_keyword_names()
+            self.assertEquals(actual_keywords, expected_keywords)
+
+            # fmt: off
+            # This is much easier to read than if black were to reformat it.
+            expected_registry = {
+                ("Detail", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.DetailPage'>",
+                ("Edit", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.EditModal'>",
+                ("Home", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.HomePage'>",
+                ("Listing", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.ListingPage'>",
+                ("New", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.NewModal'>",
+                ("ObjectManager", ""): "<class 'cumulusci.robotframework.pageobjects.ObjectManagerPageObject.ObjectManagerPage'>",
+            }
+            # fmt: on
+            actual_registry = {key: repr(value) for key, value in po.registry.items()}
+            self.assertEqual(actual_registry, expected_registry)
 
     def test_file_in_pythonpath(self, get_context_mock, get_library_instance_mock):
         """Verify we can find a page object via PYTHONPATH"""
         # PageObjects will throw an error if it can't find the file.
         # As long as this doesn't throw an error, we're golden.
-        if HERE not in sys.path:
-            sys.path.append(HERE)
-        PageObjects("FooTestPage.py")
+        try:
+            sys_path = sys.path
+            if HERE not in sys.path:
+                sys.path.append(HERE)
+
+            PageObjects("FooTestPage.py")
+        finally:
+            sys.path = sys_path
 
     def test_exception_not_found(self, get_context_mock, get_library_instance_mock):
         """Verify we get an assertion of we can't find a page object file"""
-        # make sure the folder isn't on system path by accident
-        if HERE in sys.path:
-            sys.path.remove(HERE)
         with pytest.raises(
-            ImportError, match="Unable to find page object file 'FooTestPage.py'"
+            ImportError, match="Unable to find page object file 'bogus.py'"
         ):
-            PageObjects("FooTestPage.py")
+            PageObjects("bogus.py")
 
     def test_import_failed(self, get_context_mock, get_library_instance_mock):
         with temporary_dir() as d:
             with open("busted.py", "w") as f:
                 f.write("class Busted  # incomplete class\n")
                 f.close()
-                sys.path.append(d)
-                with pytest.raises(
-                    ImportError, match="Unable to import page object 'busted.py': .*"
-                ):
-                    PageObjects("busted.py")
+                try:
+                    sys_path = sys.path
+                    sys.path.append(d)
+                    with pytest.raises(
+                        ImportError,
+                        match="Unable to import page object 'busted.py': .*",
+                    ):
+                        PageObjects("busted.py")
+                finally:
+                    sys.path = sys_path
 
     def test_PageObject_registry_with_custom_pageobjects(
         self, get_context_mock, get_library_instance_mock
     ):
         """Verify that custom page objects get added to the registry"""
-        po = PageObjects(FOO_PATH, BAR_PATH)
-
-        # The page object class will have been imported by robot.utils.Importer,
-        # so we need to use that here to validate which class got imported.
-        FooTestPage = importer.import_class_or_module_by_path(FOO_PATH)
-        BarTestPage = importer.import_class_or_module_by_path(BAR_PATH)
-
-        expected_registry = BASE_REGISTRY
-        expected_registry.update(
-            {("Test", "Foo__c"): FooTestPage, ("Test", "Bar__c"): BarTestPage}
-        )
-
-        self.assertEqual(po.registry, expected_registry)
+        with reload_PageObjects(FOO_PATH, BAR_PATH) as po:
+            # fmt: off
+            # This is much easier to read than if black were to reformat it.
+            expected_registry = {
+                ("Detail", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.DetailPage'>",
+                ("Edit", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.EditModal'>",
+                ("Home", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.HomePage'>",
+                ("Listing", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.ListingPage'>",
+                ("New", ""): "<class 'cumulusci.robotframework.pageobjects.BasePageObjects.NewModal'>",
+                ("ObjectManager", ""): "<class 'cumulusci.robotframework.pageobjects.ObjectManagerPageObject.ObjectManagerPage'>",
+                ("Test", "Bar__c"): "<class 'BarTestPage.BarTestPage'>",
+                ("Test", "Foo__c"): "<class 'FooTestPage.FooTestPage'>",
+            }
+            # fmt: on
+            actual_registry = {key: repr(value) for key, value in po.registry.items()}
+            self.assertEqual(actual_registry, expected_registry)
 
     def test_namespaced_object_name(self, get_context_mock, get_library_instance_mock):
         """Verify that the object name is prefixed by the namespace when there's a namespace"""
         with mock.patch.object(
             CumulusCI, "get_namespace_prefix", return_value="foobar__"
         ):
-            po = PageObjects(FOO_PATH)
+            with reload_PageObjects(FOO_PATH) as po:
 
-            FooTestPage = importer.import_class_or_module_by_path(FOO_PATH)
-            MockGetLibraryInstance.libs["FooTestPage"] = _PageObjectLibrary(
-                FooTestPage()
-            )
+                FooTestPage = importer.import_class_or_module_by_path(FOO_PATH)
+                MockGetLibraryInstance.libs["FooTestPage"] = _PageObjectLibrary(
+                    FooTestPage()
+                )
 
-            pobj = po.get_page_object("Test", "Foo__c")
-            self.assertEqual(pobj.object_name, "foobar__Foo__c")
+                pobj = po.get_page_object("Test", "Foo__c")
+                self.assertEqual(pobj.object_name, "foobar__Foo__c")
 
     def test_non_namespaced_object_name(
         self, get_context_mock, get_library_instance_mock
     ):
         """Verify that the object name is not prefixed by a namespace when there is no namespace"""
         with mock.patch.object(CumulusCI, "get_namespace_prefix", return_value=""):
-            po = PageObjects(FOO_PATH)
+            with reload_PageObjects(FOO_PATH) as po:
 
-            FooTestPage = importer.import_class_or_module_by_path(FOO_PATH)
-            MockGetLibraryInstance.libs["FooTestPage"] = _PageObjectLibrary(
-                FooTestPage()
-            )
+                FooTestPage = importer.import_class_or_module_by_path(FOO_PATH)
+                MockGetLibraryInstance.libs["FooTestPage"] = _PageObjectLibrary(
+                    FooTestPage()
+                )
 
-            pobj = po.get_page_object("Test", "Foo__c")
-            self.assertEqual(pobj.object_name, "Foo__c")
+                pobj = po.get_page_object("Test", "Foo__c")
+                self.assertEqual(pobj.object_name, "Foo__c")
 
 
 @mock.patch(
@@ -189,6 +209,8 @@ class TestBasePage(unittest.TestCase):
         selib = BuiltIn().get_library_instance("SeleniumLibrary")
         selib.set_selenium_implicit_wait.return_value = 7
         selib.set_selenium_implicit_wait.reset_mock()
+
+        from cumulusci.robotframework.pageobjects import BasePage
 
         page = BasePage()
         with page._no_implicit_wait():
@@ -208,6 +230,8 @@ class TestBasePage(unittest.TestCase):
         selib = BuiltIn().get_library_instance("SeleniumLibrary")
         selib.set_selenium_implicit_wait.return_value = 42
         selib.set_selenium_implicit_wait.reset_mock()
+
+        from cumulusci.robotframework.pageobjects import BasePage
 
         page = BasePage()
         try:
