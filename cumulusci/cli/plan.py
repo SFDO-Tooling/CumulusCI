0fde3d00dc2cda5b8dd971382a587977f27b5c85
Bryan Oakley <bryan.oakley@gmail.com>
2021-10-18 18:35:21 -0500

    set plan to empty dict if there are no plans also tweaked some documentation strings

--- cumulusci/cli/plan.py
+++ cumulusci/cli/plan.py
@@ -8,17 +8,22 @@ from cumulusci.cli.ui import CliTable
 from .runtime import pass_runtime
 
 
-@click.group("plan", help="Commands for getting information about metadeploy plans")
+@click.group("plan", help="Commands for getting information about MetaDeploy plans")
 def plan():
     pass
 
 
-@plan.command(name="list", help="List available plans for the current context")
+@plan.command(name="list")
 @click.option("--verbose", "verbose", is_flag=True, help="Verbose mode")
 @click.option("--json", "print_json", is_flag=True, help="Print a json string")
 @pass_runtime(require_project=True, require_keychain=True)
 def plan_list(runtime, print_json, verbose):
-    plans = runtime.project_config.plans
+    """List available plans for the current context."""
+
+    # when there are no plans, `project_config.plans` is an empty
+    # list rather than empty dictionary. I don't know why that is,
+    # but I need it to be an empty dictionary.
+    plans = runtime.project_config.plans or {}
     columns = {"title": "Title", "slug": "Plan Slug", "tier": "Tier"}
     if verbose:
         columns.update(
