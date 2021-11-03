97bbb1a87af94b2d94168a6d500d8303e23e5c34
Brandon Parker <brandon.parker@salesforce.com>
2021-11-03 13:40:45 -0700

    remove references to python 3.6 & 3.7

--- setup.py
+++ setup.py
@@ -63,8 +63,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
