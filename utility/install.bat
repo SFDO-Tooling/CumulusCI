2dedfdcf669f8a87a32522e55dbc9e446e5e8eb2
Paul Prescod <pprescod@salesforce.com>
2021-11-11 10:30:29 -0800

    Nested installer

---
+++ utility/install.bat
@@ -0,0 +1,2 @@
+%~dp0\python\python %~dp0\python\venv_from_pip.py
+pause "Done installing"
--- utility/install.sh
+++ utility/install.sh
@@ -1,3 +1,3 @@
 set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-$SCRIPT_DIR/bin/python3 $SCRIPT_DIR/bin/venv_from_pip.py
+$SCRIPT_DIR/python//bin/python3 $SCRIPT_DIR/python/bin/venv_from_pip.py
