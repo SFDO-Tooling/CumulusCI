685ca5c3782b586f87a1030566e24b850676b560
Paul Prescod <pprescod@salesforce.com>
2021-11-12 10:01:22 -0800

    Turn off Apple's security warnings for Proof of Concept

--- utility/install.sh
+++ utility/install.sh
@@ -1,3 +1,5 @@
 set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+xattr -dr com.apple.quarantine $SCRIPT_DIR  # turn off Apple's quarantine warnings
+                                            # TODO: review this with Security!
 $SCRIPT_DIR/python//bin/python3 $SCRIPT_DIR/python/bin/venv_from_pip.py
