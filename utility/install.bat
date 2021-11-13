cc4a55dd7a2a3810641a25526b1db06ecb2a9aa6
Paul Prescod <pprescod@salesforce.com>
2021-11-13 11:31:01 -0800

    Make script more reliable

--- utility/install.bat
+++ utility/install.bat
@@ -1,4 +1,4 @@
-%~dp0\python\bin\python %~dp0\python\venv_from_pip.py
-set CCIBIN=%userprofile%\.cumulusci\cci_python_env\bin
+"%~dp0\python\bin\python" "%~dp0\python\venv_from_pip.py"
+set CCIBIN="%userprofile%\.cumulusci\cci_python_env\bin"
 %CCIBIN%\python %CCIBIN%\ensure_dir_in_path.py %CCIBIN%\..\run
 pause "Done installing"
