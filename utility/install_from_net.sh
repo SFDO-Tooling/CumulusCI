2e3aece9557d4795de8d8dc63ecb3be377baa8b1
Paul Prescod <pprescod@salesforce.com>
2021-12-01 10:10:48 -0800

    Add retries to download

--- utility/install_from_net.sh
+++ utility/install_from_net.sh
@@ -13,10 +13,23 @@ fi
 
 artifactnum=$1
 github_token=$PUBLIC_GH_REPO_TOKEN
-# github_token=`cci service info github default --plain | grep token | python -c "import sys; print(sys.stdin.read().split()[1])"`
-curl -L -H "Authorization: token $github_token"  https://api.github.com/repos/SFDO-Tooling/CumulusCI/actions/artifacts/$artifactnum/zip --output /tmp/cci_installer.zip
-unzip -o /tmp/cci_installer.zip -d /tmp/cci_installer_tmp
+
+cciurl=https://api.github.com/repos/SFDO-Tooling/CumulusCI/actions/artifacts/$artifactnum/zip
+zip_path=/tmp/cci_installer.zip
+alias download_cci="curl -L -H \"Authorization: token $github_token\" $cciurl  --output $zip_path"
+
+retries=3
+while ((retries > 0)); do
+    download_cci &&
+      unzip -t $zip_path && break || echo "Fail" # fake success
+
+    echo "Retrying faied download" 
+
+    ((retries --))
+done
+
+unzip -o $zip_path -d /tmp/cci_installer_tmp
 unzip -o /tmp/cci_installer_tmp/cci_installer_mac.zip -d /tmp/cci_installer_tmp
 sh /tmp/cci_installer_tmp/cci_installer/install.sh
-rm -rf /tmp/cci_installer_tmp /tmp/cci_installer.zip
+rm -rf /tmp/cci_installer_tmp $zip_path
 /Users/pprescod/.cumulusci/cci_python_env/run/cci version
