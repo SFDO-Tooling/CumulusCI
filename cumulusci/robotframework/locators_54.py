397d38da08eeb7114a9a673d785c8cc68672723d
Bryan Oakley <bryan.oakley@gmail.com>
2021-12-22 12:14:34 -0600

    Fix for Spring '22 header link locators

---
+++ cumulusci/robotframework/locators_54.py
@@ -0,0 +1,13 @@
+import copy
+
+from cumulusci.robotframework import locators_53
+
+lex_locators = copy.deepcopy(locators_53.lex_locators)
+lex_locators["record"]["header"]["field_value"] = (
+    "//records-lwc-highlights-panel"
+    "//records-highlights-details-item[.//*[contains(@class, 'slds-text-title') and text()='{}']]"
+    "//p[contains(@class, 'fieldComponent')]//*[text()]"
+)
+lex_locators["record"]["header"][
+    "field_value_link"
+] = "//records-lwc-highlights-panel//records-highlights-details-item[.//*[.='{}']]//a"
