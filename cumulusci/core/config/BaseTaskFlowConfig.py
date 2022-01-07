7e7223fb3950b869fce2579c9aa8855316b67228
Paul Prescod <pprescod@salesforce.com>
2022-01-07 14:42:26 -0800

    Add backwards-compatible files and warnings

---
+++ cumulusci/core/config/BaseTaskFlowConfig.py
@@ -0,0 +1,7 @@
+from warnings import warn
+
+from .base_task_flow_config import *  # noqa
+
+warn(
+    "Deprecation warning: Please import from .base_task_flow_config rather than .BaseTaskFlowConfig"
+)
---
+++ cumulusci/core/config/OrgConfig.py
@@ -0,0 +1,5 @@
+from warnings import warn
+
+from .org_config import *  # noqa
+
+warn("Deprecation warning: Please import from .org_config rather than .OrgConfig")
