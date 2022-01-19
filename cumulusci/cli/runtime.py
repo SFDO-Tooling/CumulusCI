98a344e9be44d2c81cd4439fbeb05aea17316916
David Glick <dglick@salesforce.com>
2022-01-19 17:21:27 -0500

    remove new param that ended up unused

--- cumulusci/cli/runtime.py
+++ cumulusci/cli/runtime.py
@@ -157,10 +157,10 @@ class CliRuntime(BaseCumulusCI):
             else self.universal_config.list_tasks()
         )
 
-    def get_available_flows(self, project=True):
+    def get_available_flows(self):
         return (
             self.project_config.list_flows()
-            if project and self.project_config is not None
+            if self.project_config is not None
             else self.universal_config.list_flows()
         )
 
