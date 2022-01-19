ad8175743bab5431257aa5e6c0fa7d3bc6242e4b
Brandon Parker <brandon.parker@salesforce.com>
2022-01-19 14:27:36 -0800

    log payload on success

--- cumulusci/tasks/marketing_cloud/get_user_info.py
+++ cumulusci/tasks/marketing_cloud/get_user_info.py
@@ -1,5 +1,3 @@
-from logging import getLogger
-
 import requests
 
 from cumulusci.utils.http.requests_utils import safe_json_from_response
@@ -11,8 +9,6 @@ from .mc_constants import MC_API_VERSION
 class GetUserInfoTask(BaseMarketingCloudTask):
     """Retrieves user info of pertaining to the currently logged in user.
     Sanitizes and returns the payload in self.return_values."""
-
-    logger = getLogger(__name__)
 
     def _run_task(self):
         endpoint = f"https://{self.mc_config.tssd}.auth.marketingcloudapis.com/{MC_API_VERSION}/userinfo"
@@ -27,9 +23,13 @@ class GetUserInfoTask(BaseMarketingCloudTask):
             self.logger.error(f"Exception occurred fetching user info: {response.text}")
             raise
 
-        self.logger.info("Successfully fetched user info")
         payload = safe_json_from_response(response)
-        self.return_values = self._sanitize_payload(payload)
+        payload = self._sanitize_payload(payload)
+
+        self.logger.info("Successfully fetched user info.")
+        self.logger.info(payload)
+
+        self.return_values = payload
 
     def _sanitize_payload(self, payload: dict) -> dict:
         """Removes any sensitive or non-pertinent informaiton from the payload.
