ecbaa68b14cf3cf822c23baac7efddb4d3a704eb
Paul Prescod <pprescod@salesforce.com>
2022-02-14 18:27:42 -0800

    Run test 1000 times

--- cumulusci/tasks/bulkdata/tests/test_snowfakery.py
+++ cumulusci/tasks/bulkdata/tests/test_snowfakery.py
@@ -474,7 +474,7 @@ class TestSnowfakery:
             task()
         assert "Using 11 workers" in str(logger.mock_calls)
 
-    @pytest.mark.parametrize("execution_number", range(100))
+    @pytest.mark.parametrize("execution_number", range(1000))
     def test_record_count(self, snowfakery, mock_load_data, execution_number):
         task = snowfakery(recipe="datasets/recipe.yml", run_until_recipe_repeated="4")
         with mock.patch.object(task, "logger") as logger, mock.patch.object(
