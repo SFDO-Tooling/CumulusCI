ff1c66fa538d1ba8cbfd7d472a53bc3fbf21a6b3
James Estevez <jestevez@salesforce.com>
2022-02-21 16:43:43 -0800

    Warn Windows users when long paths are not enabled (#3069)
    * Add utility to check for win32 long path support
    
    This commit adds a utility function to report whether the environment
    supports long pathnames and warn the user if they are not. 
    
    Adds debugging output to cci error gist including long path support.
    
    When running error info, a warning is generated if long path support is
    not enabled.
    
    Co-authored-by: Paul Prescod <pprescod@salesforce.com>

--- cumulusci/cli/tests/test_utils.py
+++ cumulusci/cli/tests/test_utils.py
@@ -4,6 +4,7 @@ import time
 from unittest import mock
 
 import pkg_resources
+import pytest
 import requests
 import responses
 
@@ -65,3 +66,57 @@ def test_check_latest_version_request_error(click, get_latest_final_version):
     utils.check_latest_version()
 
     click.echo.assert_any_call("Error checking cci version:", err=True)
+
+
+@pytest.mark.skipif(
+    not sys.platform.startswith("win"), reason="Requires Windows Registry"
+)
+@mock.patch("winreg.QueryValueEx")
+def test_win32_warning(query_value):
+    query_value.return_value = (1, 1)
+
+    is_enabled = utils.win32_long_paths_enabled()
+
+    query_value.assert_called_once()
+    assert "LongPathsEnabled" in query_value.call_args.args
+    assert is_enabled
+
+
+@pytest.mark.skipif(
+    sys.platform.startswith("win"), reason="Should not print on non-win32"
+)
+@mock.patch("cumulusci.cli.utils.win32_long_paths_enabled")
+@mock.patch("rich.console.Console.print")
+def test_no_longpath_warn_on_posix(console_print, is_enabled):
+    utils.warn_if_no_long_paths()
+
+    is_enabled.assert_not_called()
+    console_print.assert_not_called()
+
+
+@pytest.mark.skipif(
+    not sys.platform.startswith("win"), reason="Requires Windows Registry"
+)
+@mock.patch("cumulusci.cli.utils.win32_long_paths_enabled")
+@mock.patch("rich.console.Console.print")
+def test_no_longpath_warn_on_win(console_print, is_enabled):
+    is_enabled.return_value = True
+
+    utils.warn_if_no_long_paths()
+
+    is_enabled.assert_called_once()
+    console_print.assert_not_called()
+
+
+@pytest.mark.skipif(
+    not sys.platform.startswith("win"), reason="Requires Windows Registry"
+)
+@mock.patch("cumulusci.cli.utils.win32_long_paths_enabled")
+@mock.patch("rich.console.Console.print")
+def test_longpath_warn_on_win(console_print, is_enabled):
+    is_enabled.return_value = False
+
+    utils.warn_if_no_long_paths()
+
+    is_enabled.assert_called_once()
+    console_print.assert_called_once_with(utils.WIN_LONG_PATH_WARNING)
