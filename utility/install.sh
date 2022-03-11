39087baf96363864560d37dd32deb14e2c4260a5
Paul Prescod <pprescod@salesforce.com>
2022-03-11 09:34:32 -0800

    See what happens without quarantine thing

--- utility/install.sh
+++ utility/install.sh
@@ -1,7 +1,7 @@
 set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-xattr -dr com.apple.quarantine $SCRIPT_DIR  # turn off Apple's quarantine warnings
-                                            # TODO: review this with Security!
+# xattr -dr com.apple.quarantine $SCRIPT_DIR  # turn off Apple's quarantine warnings
+#                                             # TODO: review this with Security!
 $SCRIPT_DIR/python/bin/python3 $SCRIPT_DIR/python/bin/venv_from_pip.py
 CCIBIN=~/.cumulusci/cci_python_env/bin/
 RUNDIR=~/.cumulusci/cci_python_env/run/
