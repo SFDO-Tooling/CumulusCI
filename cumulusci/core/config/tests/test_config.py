b107ef87bce17a9a113da2796a0ed06e2c2b4fac
David Reed <d.reed@salesforce.com>
2022-06-03 19:31:07 -0600

    Add property in OrgConfig to check for advanced survey features (#3149)
    Co-authored-by: James Estevez <jestevez@salesforce.com>

--- cumulusci/core/config/tests/test_config.py
+++ cumulusci/core/config/tests/test_config.py
@@ -1661,6 +1661,56 @@ class TestOrgConfig:
         # We should have made 2 calls: 1 token call + 1 describe call
         assert len(responses.calls) == 1 + 1
 
+    @responses.activate
+    def test_is_survey_advanced_features_enabled(self):
+        config = OrgConfig(
+            {
+                "instance_url": "https://example.com",
+                "access_token": "TOKEN",
+                "id": "OODxxxxxxxxxxxx/user",
+            },
+            "test",
+        )
+
+        # Token call.
+        responses.add(
+            "GET", "https://example.com/services/data", json=[{"version": 48.0}]
+        )
+
+        # describe()
+        responses.add(
+            "GET",
+            "https://example.com/services/data/v48.0/sobjects/PermissionSet/describe",
+            json={"fields": [{"name": "PermissionsAllowSurveyAdvancedFeatures"}]},
+        )
+
+        assert config.is_survey_advanced_features_enabled
+
+    @responses.activate
+    def test_is_survey_advanced_features_enabled__not_enabled(self):
+        config = OrgConfig(
+            {
+                "instance_url": "https://example.com",
+                "access_token": "TOKEN",
+                "id": "OODxxxxxxxxxxxx/user",
+            },
+            "test",
+        )
+
+        # Token call.
+        responses.add(
+            "GET", "https://example.com/services/data", json=[{"version": 48.0}]
+        )
+
+        # describe()
+        responses.add(
+            "GET",
+            "https://example.com/services/data/v48.0/sobjects/PermissionSet/describe",
+            json={"fields": [{"name": "foo"}]},
+        )
+
+        assert not config.is_survey_advanced_features_enabled
+
     def test_resolve_04t_dependencies(self):
         config = OrgConfig({}, "test")
         config._installed_packages = {
