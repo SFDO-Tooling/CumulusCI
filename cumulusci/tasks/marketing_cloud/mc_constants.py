203ba62a7cd814dd91a86ebda643f04731ff6672
Brandon Parker <brandon.parker@salesforce.com>
2022-06-16 07:25:59 -0700

    revert 3198

--- cumulusci/tasks/marketing_cloud/mc_constants.py
+++ cumulusci/tasks/marketing_cloud/mc_constants.py
@@ -1,5 +1,1 @@
 MC_API_VERSION = "v2"
-
-# We *do not* want to derive this URL from `rest_instance_url` because
-# it could potentially change in the future.
-AUTH_URI = "https://{}.auth.marketingcloudapis.com"
