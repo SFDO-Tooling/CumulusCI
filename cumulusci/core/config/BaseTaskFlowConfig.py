5573f15b5287918cf69e7fdd9e0030436ad2daf7
Paul Prescod <pprescod@salesforce.com>
2022-06-27 11:46:36 -0700

    Systemetize module-moved warnings (#3250)

--- cumulusci/core/config/BaseTaskFlowConfig.py
+++ cumulusci/core/config/BaseTaskFlowConfig.py
@@ -1,7 +1,5 @@
-from warnings import warn
+from cumulusci.utils.deprecation import warn_moved
 
 from .base_task_flow_config import *  # noqa
 
-warn(
-    "Deprecation warning: Please import from .base_task_flow_config rather than .BaseTaskFlowConfig"
-)
+warn_moved(".base_task_flow_config", ".BaseTaskFlowConfig")
--- cumulusci/tasks/salesforce/license_preflights.py
+++ cumulusci/tasks/salesforce/license_preflights.py
@@ -1,2 +1,8 @@
 # for backwards compatibility
 from cumulusci.tasks.preflight.licenses import *  # noqa
+from cumulusci.utils.deprecation import warn_moved
+
+warn_moved(
+    "cumulusci.tasks.preflight.licenses",
+    __name__,
+)
