3d202466b7ae1f9fda58a0443740bd098dc6997e
Beth Breisnes <bbreisnes@salesforce.com>
2022-07-19 16:14:49 -0700

    parametrized tests

--- cumulusci/tasks/vlocity/tests/test_vlocity.py
+++ cumulusci/tasks/vlocity/tests/test_vlocity.py
@@ -8,6 +8,7 @@ from cumulusci.core.config.scratch_org_config import ScratchOrgConfig
 from cumulusci.tasks.vlocity.exceptions import BuildToolMissingError
 from cumulusci.tasks.vlocity.vlocity import (
     BUILD_TOOL_MISSING_ERROR,
+    VlocityDeployTask,
     VlocityRetrieveTask,
 )
 
@@ -37,59 +38,60 @@ def test_vlocity_build_tool_missing(project_config):
             task._init_task()
 
 
-def test_vlocity_retrieve__scratch(project_config):
-    username = "foo"
-    org_name = "dev"
-    org_config = ScratchOrgConfig(
-        {
-            "instance_url": "https://test.salesforce.com",
-            "username": username,
-            "org_id": "00Dxxxxxxxxxxxx",
-            "password": "test",
-        },
-        org_name,
-        keychain=mock.Mock(),
-    )
+username = "foo"
+org_name = "dev"
+access_token = "foo.bar.baz"
+instance_url = "https://something.custom.salesforce.com"
+scratch_org_config = ScratchOrgConfig(
+    {
+        "instance_url": "https://test.salesforce.com",
+        "username": username,
+        "org_id": "00Dxxxxxxxxxxxx",
+        "password": "test",
+    },
+    org_name,
+    keychain=mock.Mock(),
+)
+persistent_org_config = OrgConfig(
+    {
+        "instance_url": instance_url,
+        "username": username,
+        "org_id": "00Dxxxxxxxxxxxx",
+        "access_token": access_token,
+    },
+    org_name,
+    keychain=mock.Mock(),
+)
+vlocity_test_cases = [
+    (
+        scratch_org_config,
+        VlocityRetrieveTask,
+        f"vlocity packExport -job vlocity.yaml -sfdx.username '{username}'",
+    ),
+    (
+        persistent_org_config,
+        VlocityRetrieveTask,
+        f"vlocity packExport -job vlocity.yaml -sf.accesstoken '{access_token}' -sf.instanceUrl '{instance_url}'",
+    ),
+    (
+        scratch_org_config,
+        VlocityDeployTask,
+        f"vlocity packDeploy -job vlocity.yaml -sfdx.username '{username}'",
+    ),
+    (
+        persistent_org_config,
+        VlocityDeployTask,
+        f"vlocity packDeploy -job vlocity.yaml -sf.accesstoken '{access_token}' -sf.instanceUrl '{instance_url}'",
+    ),
+]
+
+
+@pytest.mark.parametrize("org_config,task_class,expected_command", vlocity_test_cases)
+def test_vlocity_simple_job(project_config, org_config, task_class, expected_command):
+
     task_config = TaskConfig(
         config={"options": {"job_file": "vlocity.yaml", "org": org_name}}
     )
-    task = VlocityRetrieveTask(project_config, task_config, org_config)
+    task = task_class(project_config, task_config, org_config)
 
-    assert (
-        task._get_command()
-        == f"vlocity packExport -job vlocity.yaml -sfdx.username '{username}'"
-    )
-
-
-def test_vlocity_retrieve__persistent(project_config):
-    username = "foo"
-    org_name = "dev"
-    access_token = "foo.bar.baz"
-    instance_url = "https://something.custom.salesforce.com"
-    org_config = OrgConfig(
-        {
-            "instance_url": instance_url,
-            "username": username,
-            "org_id": "00Dxxxxxxxxxxxx",
-            "access_token": access_token,
-        },
-        org_name,
-        keychain=mock.Mock(),
-    )
-    task_config = TaskConfig(
-        config={"options": {"job_file": "vlocity.yaml", "org": org_name}}
-    )
-    task = VlocityRetrieveTask(project_config, task_config, org_config)
-
-    assert (
-        task._get_command()
-        == f"vlocity packExport -job vlocity.yaml -sf.accesstoken '{access_token}' -sf.instanceUrl '{instance_url}'"
-    )
-
-
-def test_vlocity_deploy__scratch(project_config):
-    pass
-
-
-def test_vlocity_deploy__persistent(project_config):
-    pass
+    assert task._get_command() == expected_command
