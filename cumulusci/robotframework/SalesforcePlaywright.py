e25488027418a92e5700238a60f95697923888c4
Bryan Oakley <bryan.oakley@gmail.com>
2022-07-28 17:18:51 -0500

    Add support for get_current_record_id This keyword works with delete_records_and_close_browser. Adding this gives SalesforcePlaywright feature parity with Salesforce with respect to managing record ids.

--- cumulusci/robotframework/SalesforcePlaywright.py
+++ cumulusci/robotframework/SalesforcePlaywright.py
@@ -1,7 +1,10 @@
+import re
+
 from Browser import SupportedBrowsers
 
 from cumulusci.robotframework.base_library import BaseLibrary
 from cumulusci.robotframework.faker_mixin import FakerMixin
+from cumulusci.robotframework.utils import WAIT_FOR_AURA_SCRIPT
 
 
 class SalesforcePlaywright(FakerMixin, BaseLibrary):
@@ -16,6 +19,29 @@ class SalesforcePlaywright(FakerMixin, BaseLibrary):
         if self._browser is None:
             self._browser = self.builtin.get_library_instance("Browser")
         return self._browser
+
+    def get_current_record_id(self):
+        """Parses the current url to get the object id of the current record.
+        This expects the url to contain an id that matches [a-zA-Z0-9]{15,18}
+        """
+        OID_REGEX = r"^(%2F)?([a-zA-Z0-9]{15,18})$"
+        url = self.browser.execute_javascript("window.location.href")
+        for part in url.split("/"):
+            oid_match = re.match(OID_REGEX, part)
+            if oid_match is not None:
+                return oid_match.group(2)
+        raise AssertionError("Could not parse record id from url: {}".format(url))
+
+    def go_to_record_home(self, obj_id):
+        """Navigates to the Home view of a Salesforce Object
+
+        After navigating, this will wait until the slds-page-header_record-home
+        div can be found on the page.
+        """
+        url = self.cumulusci.org.lightning_base_url
+        url = "{}/lightning/r/{}/view".format(url, obj_id)
+        self.browser.go_to(url)
+        self.wait_until_loading_is_complete("div.slds-page-header_record-home")
 
     def delete_records_and_close_browser(self):
         """This will close all open browser windows and then delete
@@ -83,3 +109,32 @@ class SalesforcePlaywright(FakerMixin, BaseLibrary):
         self.browser.wait_until_network_is_idle()
 
         return browser_id, context_id, page_details
+
+    def wait_until_loading_is_complete(self, locator=None):
+        """Wait for a lightning page to load.
+
+        By default this keyword will wait for any element with the
+        class 'slds-template__container', but a different locator can
+        be provided.
+
+        In addition to waiting for the element, it will also wait for
+        any pending aura events, and it also calls the Browser keyword
+        `Wait until network is idle`.
+
+        """
+        locator = (
+            "//div[contains(@class, 'slds-template__container')]/*"
+            if locator is None
+            else locator
+        )
+        try:
+            self.browser.get_element(locator)
+            self.browser.execute_javascript(function=WAIT_FOR_AURA_SCRIPT)
+            self.browser.wait_until_network_is_idle()
+
+        except Exception:
+            try:
+                self.browser.take_screenshot()
+            except Exception as e:
+                self.builtin.warn("unable to capture screenshot: {}".format(str(e)))
+            raise
