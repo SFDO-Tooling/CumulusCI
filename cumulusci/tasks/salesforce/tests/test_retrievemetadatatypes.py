f9d47ea2f5f8e25645e84f2eaffef7c0b9d53874
lakshmi2506 <l.ramireddy@salesforce.com>
2023-10-05 17:08:07 +0530

    tests_added

--- cumulusci/tasks/salesforce/tests/test_retrievemetadatatypes.py
+++ cumulusci/tasks/salesforce/tests/test_retrievemetadatatypes.py
@@ -1,9 +1,9 @@
-
-
 from unittest import mock
 
 from cumulusci.tasks.salesforce import RetrieveMetadataTypes
+
 from .util import create_task
+
 
 class TestRetrieveMetadataTypes:
     def test_run_task(self):
@@ -12,15 +12,7 @@ class TestRetrieveMetadataTypes:
         task()
         task._get_api.assert_called_once()
 
-
     def test_run_task_with_apiversion(self):
-        task = create_task(RetrieveMetadataTypes,{"api_version": 8.0})
-        assert task.options.get("api_version")==8.0
+        task = create_task(RetrieveMetadataTypes, {"api_version": 8.0})
+        assert task.options.get("api_version") == 8.0
         task._get_api()
-
-  
-        
-
-        
-        
-        
