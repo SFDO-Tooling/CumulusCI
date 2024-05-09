126da0fd4805e369c0def48677e85341b5b2134f
Jason Lantz <jason@muselab.com>
2024-05-09 09:07:14 -0500

    Update create_package_version.py

--- cumulusci/tasks/create_package_version.py
+++ cumulusci/tasks/create_package_version.py
@@ -229,7 +229,7 @@ class CreatePackageVersion(BaseSalesforceApiTask):
         package_zip_builder = None
         with convert_sfdx_source(
             self.project_config.default_package_path,
-            None
+            None,
             None,
             self.logger,
         ) as path:
