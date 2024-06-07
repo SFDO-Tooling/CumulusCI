491e242232377c88e247ce536b7b798514ab53ef
lakshmi2506 <l.ramireddy@salesforce.com>
2024-06-07 15:19:39 +0530

    Display files task added

---
+++ cumulusci/tasks/salesforce/salesforce_files.py
@@ -0,0 +1,30 @@
+from cumulusci.tasks.salesforce import BaseSalesforceApiTask
+
+
+class DisplayFiles(BaseSalesforceApiTask):
+    task_docs = """
+    Lists the available documents that has been uploaded to a library in Salesforce CRM Content or Salesforce Files
+    """
+
+    def _run_task(self):
+        self.return_values = [
+            {
+                "Id": result["Id"],
+                "Title": result["Title"],
+                "FileType": result["FileType"],
+            }
+            for result in self.sf.query(
+                "SELECT Title, Id, FileType FROM ContentDocument"
+            )["records"]
+        ]
+        self.logger.info(f"Found {len(self.return_values)} files")
+        if len(self.return_values) > 0:
+            self.logger.info(f"{'Id':<20} {'Title':<50} {'FileType':<10}")
+
+            # Print each row of the table
+            for file_desc in self.return_values:
+                self.logger.info(
+                    f"{file_desc['Id']:<20} {file_desc['Title']:<50} {file_desc['FileType']:<10}"
+                )
+
+        return self.return_values
---
+++ cumulusci/tasks/salesforce/tests/test_salesforce_files.py
@@ -0,0 +1,26 @@
+from unittest.mock import Mock
+
+from cumulusci.tasks.salesforce.salesforce_files import DisplayFiles
+from cumulusci.tasks.salesforce.tests.util import create_task
+
+
+class TestDisplayFiles:
+    def test_display_files(self):
+        task = create_task(DisplayFiles, {})
+        task._init_api = Mock()
+        task._init_api.return_value.query.return_value = {
+            "totalSize": 2,
+            "records": [
+                {"Title": "TEST1", "Id": "0PS000000000000", "FileType": "TXT"},
+                {"Title": "TEST2", "Id": "0PS000000000001", "FileType": "TXT"},
+            ],
+        }
+        task()
+
+        task._init_api.return_value.query.assert_called_once_with(
+            "SELECT Title, Id, FileType FROM ContentDocument"
+        )
+        assert task.return_values == [
+            {"Id": "0PS000000000000", "Title": "TEST1", "FileType": "TXT"},
+            {"Id": "0PS000000000001", "Title": "TEST2", "FileType": "TXT"},
+        ]
