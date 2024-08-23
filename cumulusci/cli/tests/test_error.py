c3103c94d5a57098d9e1058427033ba2f3b04e47
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2024-08-23 08:08:57 +0530

    Test test_error.py

--- cumulusci/cli/tests/test_error.py
+++ cumulusci/cli/tests/test_error.py
@@ -117,7 +117,7 @@ Environment Info: Rossian / x68_46
 
         class ExceptionWithResponse(Exception, mock.Mock):
             def __init__(self, status_code):
-                super(ExceptionWithResponse, self).__init__() 
+                super(Exception, self).__init__() 
                 self.response = mock.Mock(status_code=status_code)
 
         create_gist.side_effect = ExceptionWithResponse(503)
