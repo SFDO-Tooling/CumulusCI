d33ecd3073f87f424738063de53cd0a53c5bd2de
James Estevez <jestevez@salesforce.com>
2025-10-10 11:23:24 -0700

    fix: Remove unused global declaration
    Remove the global declaration for 'has_shown_yaml_error_message'.
    This variable was deleted in a previous change, but the declaration
    was left behind by mistake. This fixes flake8 error F824.

--- cumulusci/utils/yaml/cumulusci_yml.py
+++ cumulusci/utils/yaml/cumulusci_yml.py
@@ -280,7 +280,6 @@ class ErrorDict(TypedDict):
 
 def _log_yaml_errors(logger, errors: List[ErrorDict]):
     "Format and log a Pydantic-style error dictionary"
-    global has_shown_yaml_error_message
     plural = "" if len(errors) <= 1 else "s"
     logger.warning(f"CumulusCI Configuration Warning{plural}:")
     for error in errors:
